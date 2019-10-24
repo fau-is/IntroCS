8e193e5c00b92a579ff82a9f13fea357f11cca51
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-24 16:01:19 +0200

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int to_search[10] = {1, 11, 4, 5, 9, 12, 55, 78, 0, 7};
+int to_search[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
 int main(int argc, char **argv)
 {
@@ -12,5 +12,37 @@ int main(int argc, char **argv)
     }
     int to_find = atoi(argv[1]);
     int array_length = sizeof(to_search)/sizeof(to_search[0]);
-    \\ TODO
+    
+    int first = 0;
+    int last = array_length - 1;
+    int middle = last / 2; 
+    
+    // As long as the last element to examine is equal 
+    // or lower than the first element proceed 
+    while (last >= first)
+    {
+        if (to_find == to_search[middle])
+        {
+            // If number is found print and return 0
+            printf("Number %i is at index %i\n", to_find, middle);
+            return 0;
+        }
+        else if (to_find < to_search[middle])
+        {
+            // Set last as middle -1, persuing on the side with minor numbers
+            // - 1 because we know it's not the number at present middle
+            last = middle - 1;
+        }
+        else if (to_find > to_search[middle])
+        {
+            // Set first as middle +1, persuing on the side with major numbers
+            // + 1 Because we know already it's not the number at present middle
+            first = middle + 1;
+        }
+        // Reset the middle variable
+        middle = (first + last) / 2; 
+    }
+    
+    printf("Number %i not found\n", to_find);
+    return 1;
 }
