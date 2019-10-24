cf6aaccee048604110ef8f5d5a0fde906416f762
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-24 11:41:14 +0200

    Update

--- linear.c
+++ linear.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int to_search[10] = {1, 11, 4, 5, 9, 12, 55, 78, 0, 7};
+
+int main(int argc, char **argv)
 {
-    printf("Sorted");
+    if (argc != 2)
+    {
+        printf("Usage: ./linear Number");
+        return 1;
+    }
+    int to_find = atoi(argv[1]);
+    array_length = sizeof(to_search)/sizeof(to_search[0]);
+    
+    for (int i = 0; i < array_length; i++)
+    {
+        if (to_search[i] == to_find)
+        {
+            printf("The number %i was found at index %i ", to_find, i);
+        }
+    }
+    printf("The number %i is not in the array", to_find, i);
 }
