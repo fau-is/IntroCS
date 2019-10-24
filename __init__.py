11ec410221bedd289c54b5c71a80e55b7f667e53
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-24 17:42:21 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,22 @@
+import check50
+import check50.c
+
+@check50.check()
+def compiles():
+    """binary.c compiles."""
+    check50.c.compile("binary.c", lcs50=True)
+
+@check50.check(compiles)
+def finds_10():
+    """Finds 10 in the array"""
+    check50.run("./binary 10").stdout("Number 10 is at index 0").exit(0)
+
+@check50.check(compiles)
+def finds_100():
+    """Finds 10 in the array"""
+    check50.run("./binary 100").stdout("Number 100 is at index 9").exit(0)
+
+@check50.check(compiles)
+def cannot_find_5():
+    """Test not found message with 5"""
+    check50.run("./binary 5").stdout("Number 5 not found").exit(0)
