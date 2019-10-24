8e266e5c0cf03f20a8f19783dcc2b62b6439c55b
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-24 11:59:40 +0200

    Update linear.c

--- linear.c
+++ linear.c
@@ -17,8 +17,10 @@ int main(int argc, char **argv)
     {
         if (to_search[i] == to_find)
         {
-            printf("The number %i was found at index %i ", to_find, i);
+            printf("The number %i was found at index %i\n", to_find, i);
+            return 0;
         }
     }
-    printf("The number %i is not in the array", to_find);
+    printf("The number %i is not in the array\n", to_find);
+    return 1;
 }
