541b5500ccf708895cd9aea82f102a81bb50f5c9
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-24 16:03:55 +0200

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int to_search[10] = {1, 11, 4, 5, 9, 12, 55, 78, 0, 7};
+int to_search[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
 int main(int argc, char **argv)
 {
@@ -12,5 +12,20 @@ int main(int argc, char **argv)
     }
     int to_find = atoi(argv[1]);
     int array_length = sizeof(to_search)/sizeof(to_search[0]);
-    // TODO
+    
+    // Helpers
+    // Initial index of first element
+    int first = 0;
+    //Initial index of last element
+    int last = array_length - 1;
+    // Initial middle
+    int middle = last / 2; 
+    
+    // TODO Iterative Binary Search 
+    // Hint: Maybe a while loop helps
+    
+    
+    printf("Number %i not found\n", to_find);
+    return 1;
+
 }
