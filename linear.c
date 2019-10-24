87cf4b77ac2fc6ea825852f61892c1edee882bca
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-24 17:55:02 +0200

    Update linear.c

--- linear.c
+++ linear.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        printf("Usage: ./linear Number");
+        printf("Usage: ./linear Number\n");
         return 1;
     }
     int to_find = atoi(argv[1]);
