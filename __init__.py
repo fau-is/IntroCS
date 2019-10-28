0495480b133d0eaa648c423da92dcc84a7175f9a
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-28 14:10:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,15 +8,15 @@ def compiles():
 
 @check50.check(compiles)
 def sorts_asc():
-    """Finds 10 in the array"""
+    """Sorts ascending"""
     check50.run("./bubble asc").stdout("[2, 5, 8, 23, 33, 42, 78, 123, 2398, 4711]").exit(0)
 
 @check50.check(compiles)
 def sorts_dsc():
-    """Finds 100 in the array"""
+    """Sorts descending"""
     check50.run("./bubble dsc").stdout("[4711, 2398, 123, 78, 42, 33, 23, 8, 5, 2]").exit(0)
     
 @check50.check(compiles)
 def usage():
-    """Finds 50 in the array"""
+    """Prints Usage"""
     check50.run("./bubble ots").stdout("Usage: ./bubble [asc|dsc]").exit(1)
