d8f7f4041cf3f7d26a524d966826b1a5f1204261
EinGartenzwerg <maximilian.harl@gmail.com>
2019-10-28 17:16:29 +0800

    selsort quiz

---
+++ selsort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int to_sort[10] = {4711, 8, 2, 33, 5, 23, 42, 78, 123, 2398};
+
+void pretty_printer(int *to_print)
+{
+    printf("[");
+    for (int i = 0, j = sizeof(to_sort)/sizeof(to_sort[0]); i < j; i++)
+    {
+        printf("%i, ", to_print[i]);
+    }
+    printf("]\n");
+}
+
+int main(int argc, char **argv)
+{
+    int asc = 0;
+
+    if (argc != 2)
+    {
+        printf("Usage: ./bubble [asc|dsc]\n");
+        return 1;
+    }
+   
+
+    //same as in bubble sort
+
+
+    //helpers
+    int limit = sizeof(to_sort)/sizeof(to_sort[0]);
+    int max;
+    int maxIndex;
+    //why this way? :)
+    for (int i = limit - 1; i > 0; i--)
+    {
+
+
+        //TODO
+
+    }
+
+
+    //TODO
+
+
+    return 0;
+}
