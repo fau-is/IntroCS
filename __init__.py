5c37933363202a281591079c77173f84e75158c8
EinGartenzwerg <38346167+EinGartenzwerg@users.noreply.github.com>
2019-10-28 21:29:30 +0800

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,22 @@
+import check50
+import check50.c
+
+@check50.check()
+def compiles():
+    """bubble.c compiles."""
+    check50.c.compile("heapSort.c", lcs50=True)
+
+@check50.check(compiles)
+def sorts_asc():
+    """Sorts ascending"""
+    check50.run("./heapSort asc").stdout("[2, 5, 8, 23, 33, 42, 78, 123, 2398, 4711, ]").exit(0)
+
+@check50.check(compiles)
+def sorts_dsc():
+    """Sorts descending"""
+    check50.run("./heapSort dsc").stdout("[4711, 2398, 123, 78, 42, 33, 23, 8, 5, 2, ]").exit(0)
+    
+@check50.check(compiles)
+def usage():
+    """Prints Usage"""
+    check50.run("./heapSort asdf").stdout("Usage: ./bubble [asc|dsc]\n").exit(1)
