35ae3342f1044d519f25a0ebf8f54eafea3cf3b7
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-28 17:25:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,13 +7,6 @@ def compiles():
     """babylon.c compiles."""
     check50.c.compile("babylon.c", lcs50=True)
 
-
-@check50.check(compiles)
-def finds_2():
-    """Finds that 2 is sqrt[2](4)"""
-    check50.run("./babylon 2 4").stdout("2.00000").exit(0)
-
-
 @check50.check(compiles)
 def regards_pre_req():
     """Does not ignore the prerequisites"""
@@ -21,23 +14,27 @@ def regards_pre_req():
     check50.run("./babylon 2 -4").stdout("Try again...").exit(1)
     check50.run("./babylon 3 -4").stdout("-1.58741").exit(0)
 
+@check50.check(compiles)
+def finds_2():
+    """Finds that 2 is sqrt[2](4)"""
+    check50.run("./babylon 2 4").stdout("2.00000").exit(0)
 
 @check50.check(compiles)
 def finds_88_22():
-    """Finds 4 in the array"""
+    """Finds correct root for k = 88 and a = 22"""
     check50.run("./babylon 88 22").stdout("1.04616").exit(0)
 
 
 @check50.check(compiles)
 def finds_22_88():
-    """Finds 7 in the array"""
+    """Finds correct root for k = 22 and a = 88"""
     check50.run("./babylon 22 88").stdout("1.22573").exit(0)
 
 
 @check50.check(compiles)
 def finds_69_42():
-    """Test not found message with 3"""
-    check50.run("./linear 3").stdout("1.10709").exit(0)
+    """Finds the right root for k = 69 and a = 42 """
+    check50.run("./babylon 69 42").stdout("1.10709").exit(0)
 
 
 @check50.check(compiles)
