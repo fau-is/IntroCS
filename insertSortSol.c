3222852640a42ef4b21f56d7c79ee6a21e1b7575
EinGartenzwerg <maximilian.harl@gmail.com>
2019-10-29 03:38:03 +0800

    insertion sort sol

---
+++ insertSortSol.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+char *to_sort = "zdvixqcuvd";
+//the same as "zdvixqcuvd"
+
+void pretty_printer(char *to_print, int size)
+{
+    printf("[");
+    for (int i = 0; i < size; i++)
+    {
+        if (size - i > 1)
+        {
+            printf("%c, ", to_print[i]);
+        }
+        else
+        {
+            printf("%c", to_print[i]);
+        }
+    }
+    printf("]\n");
+}
+
+int main(int argc, char **argv)
+{
+    int asc = 0;
+
+    if (argc != 3)
+    {
+        printf("Usage: ./bubble [asc|dsc]\n");
+        return 1;
+    }
+    if (strcmp(argv[1], "asc") == 0)
+    {
+        asc = 1;
+    }
+    else if (strcmp(argv[1], "dsc") == 0)
+    {
+        asc = 0;
+    }
+    else
+    {
+        printf("Usage: ./bubble [asc|dsc]\n");
+        return 1;
+    }
+    to_sort = argv[2];
+
+    //helpers
+    int size = strlen(to_sort);
+    int max;
+    int idx;
+    //why this way? :)
+    for (int i = 0 ; i < size; i++)
+    {
+        max = to_sort[i];
+        idx = i - 1;
+        bool cond = !asc ? max > to_sort[idx] : max < to_sort[idx];
+        while(idx >= 0 && cond)
+        {
+            to_sort[idx + 1] = to_sort[idx];
+            idx--;
+            cond = !asc ? max > to_sort[idx] : max < to_sort[idx];
+        }
+        to_sort[idx+1] = max;
+    }
+
+    pretty_printer(to_sort, size);
+    return 0;
+}
