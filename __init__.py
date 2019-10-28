792252355853eb0c8554a8566cdc0645488d8728
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-28 16:23:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,17 +11,17 @@ def compiles():
 @check50.check(compiles)
 def finds_usage():
     """returns usage without args"""
-    check50.run("./selsort").stdout("Usage: ./bubble [asc|dsc]").exit(1)
-    check50.run("./selsort asdf").stdout("Usage: ./bubble [asc|dsc]").exit(1)
+    check50.run("./selsort").stdout("Usage: ./bubble [asc|dsc]", regex=False).exit(1)
+    check50.run("./selsort asdf").stdout("Usage: ./bubble [asc|dsc]", regex=False).exit(1)
 
 
 @check50.check(compiles)
 def sort_asc():
     """Does not ignore the prerequisites"""
-    check50.run("./selsort asc").stdout("[2, 5, 8, 23, 33, 42, 78, 123, 2398, 4711, ]").exit(0)
+    check50.run("./selsort asc").stdout("[2, 5, 8, 23, 33, 42, 78, 123, 2398, 4711]", regex=False).exit(0)
 
 
 @check50.check(compiles)
 def sort_dsc():
     """Does not ignore the prerequisites"""
-    check50.run("./selsort dsc").stdout("[4711, 2398, 123, 78, 42, 33, 23, 8, 5, 2, ]").exit(0)
+    check50.run("./selsort dsc").stdout("[4711, 2398, 123, 78, 42, 33, 23, 8, 5, 2]", regex=False).exit(0)
