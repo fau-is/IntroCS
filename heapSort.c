2f0bf14c20667415478fb25d90694dda1e4450c5
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-10-28 15:17:17 +0100

    Update heapSort.c

--- heapSort.c
+++ heapSort.c
@@ -3,16 +3,57 @@
 #include <string.h>
 #include <stdbool.h>
 
+void pretty_printer(int *to_print, size_t size);
+void swap(int *arr, int a, int b);
+void reheap(int *to_heap, int i, int k, int asc);
+void my_heapsort(int *tb_sort, int asc, size_t size);
+
 int to_sort[10] = {4711, 8, 2, 33, 5, 23, 42, 78, 123, 2398};
 
-void pretty_printer(int *to_print, int size)
+
+int main(int argc, char **argv)
 {
-    printf("[");
-    for (int i = 0; i < size; i++)
+    int asc;
+
+    if (argc != 2)
     {
-        printf("%i, ", to_print[i]);
+        printf("Usage: ./heapSort [asc|dsc]\n");
+        return 1;
     }
-    printf("]\n");
+
+    if (strcmp(argv[1], "asc") == 0)
+    {
+        asc = 1;
+    }
+    else if (strcmp(argv[1], "dsc") == 0)
+    {
+        asc = 0;
+    }
+    else
+    {
+        printf("Usage: ./heapSort [asc|dsc]\n");
+        return 1;
+    }
+
+    //TODO: how to determine the array_length?
+
+    my_heapsort(to_sort, asc, array_length);
+
+    pretty_printer(to_sort, array_length);
+    return 0;
+}
+
+void my_heapsort(int *tb_sort, int asc, size_t size)
+{
+
+    //TODO
+    // build heap
+
+
+    //TODO
+    // take out max and reheap
+    
+
 }
 
 void swap(int *arr, int a, int b)
@@ -21,7 +62,7 @@ void swap(int *arr, int a, int b)
 }
 
 //ascending?
-//k -> here the sorted part starts, better not touch
+//k -> here the sorted part starts, better not to touch
 //i -> maybe you should sink the element at this index into the heap
 void reheap(int *to_heap, int i, int k, int asc)
 {
@@ -51,47 +92,19 @@ void reheap(int *to_heap, int i, int k, int asc)
     }
 }
 
-void my_heapsort(int *tb_sort, int asc, int size)
+void pretty_printer(int *to_print, size_t size)
 {
-
-    //TODO
-    // build heap
-
-
-    //TODO
-    // take out max and reheap
-    
-
-}
-
-int main(int argc, char **argv)
-{
-    int asc;
-
-    if (argc != 2)
+    printf("[");
+    for (int i = 0; i < size; i++)
     {
-        printf("Usage: ./bubble [asc|dsc]\n");
-        return 1;
+        if (i == size - 1)
+        {
+            printf("%i", to_print[i])   
+        }
+        else
+        {
+            printf("%i, ", to_print[i]);
+        }
     }
-
-    if (strcmp(argv[1], "asc") == 0)
-    {
-        asc = 1;
-    }
-    else if (strcmp(argv[1], "dsc") == 0)
-    {
-        asc = 0;
-    }
-    else
-    {
-        printf("Usage: ./bubble [asc|dsc]\n");
-        return 1;
-    }
-
-    //TODO: how to you determine the array_length?
-
-    my_heapsort(to_sort, asc, array_length);
-
-    pretty_printer(to_sort, array_length);
-    return 0;
+    printf("]\n");
 }
