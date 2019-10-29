e535318b72af97978e5ad2908c8b87f110647c32
EinGartenzwerg <maximilian.harl@gmail.com>
2019-10-29 14:00:08 +0800

    uiz insertsort ready

---
+++ insertSort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+char *to_sort = "zdvixqcuvd";
+//the same as "zdvixqcuvd"
+
+void pretty_printer(char *to_print, int size)
+{
+    printf("[");
+    for (int i = 0; i < size; i++)
+    {
+        if (size - i > 1)
+        {
+            printf("%c, ", to_print[i]);
+        }
+        else
+        {
+            printf("%c", to_print[i]);
+        }
+    }
+    printf("]\n");
+}
+
+int main(int argc, char **argv)
+{
+    int asc = 0;
+
+    if (argc != 3)
+    {
+        printf("Usage: ./insertSort [asc|dsc] to_sort\n");
+        return 1;
+    }
+    if (strcmp(argv[1], "asc") == 0)
+    {
+        asc = 1;
+    }
+    else if (strcmp(argv[1], "dsc") == 0)
+    {
+        asc = 0;
+    }
+    else
+    {
+        printf("Usage: ./insertSort [asc|dsc] to_sort\n");
+        return 1;
+    }
+    to_sort = argv[2];
+
+    //helpers
+    int size = strlen(to_sort);
+    int max;
+    int idx;
+    
+    for (int i = 0 ; i < size; i++)
+    {
+        //TODO
+    }
+
+    pretty_printer(to_sort, size);
+    return 0;
+}
+
