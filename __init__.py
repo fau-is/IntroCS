7475eabfd85ae3d413525b1ac9afe25275e99600
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-11-04 11:10:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,9 +10,9 @@ def compiles():
 @check50.check(compiles)
 def regards_pre_req():
     """Does not ignore the prerequisites"""
-    check50.run("./insertSort asc some string ").stdout("Usage: ./insertSort [asc|dsc]\n").exit(1)
-    check50.run("./insertSort somestring").stdout("Usage: ./insertSort [asc|dsc]\n").exit(1)
-    check50.run("./insertSort").stdout("Usage: ./insertSort [asc|dsc]\n").exit(0)
+    check50.run("./insertSort asc some string ").stdout("Usage: ./insertSort [asc|dsc] to_sort\n").exit(1)
+    check50.run("./insertSort somestring").stdout("Usage: ./insertSort [asc|dsc] to_sort\n").exit(1)
+    check50.run("./insertSort").stdout("Usage: ./insertSort [asc|dsc] to_sort\n").exit(0)
 
 @check50.check(compiles)
 def sort_introcs():
@@ -20,14 +20,9 @@ def sort_introcs():
     check50.run("./insertSort asc introcs").stdout("cinorst").exit(0)
     check50.run("./insertSort dsc introcs").stdout("tsronic").exit(0)
 
+
 @check50.check(compiles)
 def sort_IntroCS():
-    """Finds correct root for k = 88 and a = 22"""
-    check50.run("./babylon 88 22").stdout("1.04616").exit(0)
-
-
-@check50.check(compiles)
-def finds_22_88():
     """Sorts IntroCS alphabetically without capital letters"""
     check50.run("./insertSort asc IntroCS").stdout("CInorSt").exit(0)
     check50.run("./insertSort dsc IntroCS").stdout("tSronIC").exit(0)
