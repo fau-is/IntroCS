26cc34c2721a21b26d2d99d44253863b8f845808
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-11-15 12:55:45 +0100

    Update selsort_sol.c

--- selsort_sol.c
+++ selsort_sol.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 
     if (argc != 2)
     {
-        printf("Usage: ./bubble [asc|dsc]\n");
+        printf("Usage: ./selsort [asc|dsc]\n");
         return 1;
     }
     if (strcmp(argv[1], "asc") == 0)
