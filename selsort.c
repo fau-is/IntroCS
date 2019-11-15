fbed4e5cda023c944646849e741ae36eae36da7c
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2019-11-15 09:40:06 +0100

    Update selsort.c

--- selsort.c
+++ selsort.c
@@ -1,3 +1,4 @@
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,8 +6,20 @@
 
 int to_sort[10] = {4711, 8, 2, 33, 5, 23, 42, 78, 123, 2398};
 
-// Helper to print arrays
-void pretty_printer(int *to_print, int size);
+void pretty_printer(int *to_print)
+{
+    printf("[");
+    for (int i = 0, j = sizeof(to_sort)/sizeof(to_sort[0]); i < j; i++)
+    { 
+        if (i == j - 1)
+        {
+            printf("%i", to_print[i]);
+            break;
+        }
+        printf("%i, ", to_print[i]);
+    }
+    printf("]\n");
+}
 
 int main(int argc, char **argv)
 {
@@ -17,41 +30,47 @@ int main(int argc, char **argv)
         printf("Usage: ./selsort [asc|dsc]\n");
         return 1;
     }
-   
-
-    //TODO: check if sorting in asc (ascending) or dsc (descending) order
-
+    if (strcmp(argv[1], "asc") == 0)
+    {
+        asc = 1;
+    }
+    else if (strcmp(argv[1], "dsc") == 0)
+    {
+        asc = 0;
+    }
+    else
+    {
+        printf("Usage: ./selsort [asc|dsc]\n");
+        return 1;
+    }
 
     //helpers
     int limit = sizeof(to_sort)/sizeof(to_sort[0]);
     int max;
     int maxIndex;
-    //TODO implement Selection Sort
-    // HINT: We are reducing the limit from the right side on
+    //why this way? :)
     for (int i = limit - 1; i > 0; i--)
     {
-
-
-        //TODO
-
+        max = to_sort[i];
+        maxIndex = i;
+        for (int j = 0; j < i; j++)
+        {
+            bool cond = to_sort[j] >= max;
+            if (!asc)
+            {
+                cond = !cond;
+            }
+            if (cond)
+            {
+                max = to_sort[j];
+                maxIndex = j;
+            }
+        }
+        int tmp = to_sort[i];
+        to_sort[i] = to_sort[maxIndex];
+        to_sort[maxIndex] = tmp;
     }
-    pretty_printer(to_sort, 10)
+
+    pretty_printer(to_sort);
     return 0;
-}
-
-void pretty_printer(int *to_print, size_t size)
-{
-    printf("[");
-    for (int i = 0; i < size; i++)
-    {
-        if (i == size - 1)
-        {
-            printf("%i", to_print[i])   
-        }
-        else
-        {
-            printf("%i, ", to_print[i]);
-        }
-    }
-    printf("]\n");
 }
