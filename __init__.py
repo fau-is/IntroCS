0799e98c8318aeca1e1d45050832e0167ea0e407
EinGartenzwerg <maximilian.harl@gmail.com>
2019-12-24 19:42:29 +0100

    com
    com

---
+++ __init__.py
@@ -0,0 +1,37 @@
+import check50
+import check50.c
+
+@check50.check()
+def compiles():
+    """binary.c compiles."""
+    check50.c.compile("binary.c", lcs50=True)
+
+@check50.check(compiles)
+def finds_10():
+    """Finds 10 in the array"""
+    check50.run("./binary 10").stdout("Number 10 is at index 0").exit(0)
+
+@check50.check(compiles)
+def finds_100():
+    """Finds 100 in the array"""
+    check50.run("./binary 100").stdout("Number 100 is at index 9").exit(0)
+    
+@check50.check(compiles)
+def finds_50():
+    """Finds 50 in the array"""
+    check50.run("./binary 50").stdout("Number 50 is at index 4").exit(0)
+    
+@check50.check(compiles)
+def finds_60():
+    """Finds 60 in the array"""
+    check50.run("./binary 60").stdout("Number 60 is at index 5").exit(0)
+
+@check50.check(compiles)
+def cannot_find_5():
+    """Test not found message with 5"""
+    check50.run("./binary 5").stdout("Number 5 not found").exit(1)
+    
+@check50.check(compiles)
+def usage():
+    """Check usage message"""
+    check50.run("./binary").stdout("Usage: ./binary Number").exit(1)
