5457652f8b50371bdd08f4ad082ed2a195d3d367
EinGartenzwerg <maximilian.harl@gmail.com>
2019-12-24 04:24:36 +0100

    dfs impr

--- dfs_gr.py
+++ dfs_gr.py
@@ -14,23 +14,34 @@ class Graph(object):
     def add(self, start, end):
         self.adj[start].append(end)
 
-    def dfs(self, root):
+    def dfs(self, root, tofind):
+        nbr, path, found = self.dfs_helper(root, tofind)
+        if found:
+            return nbr, path
+        else
+            return -1, []
+
+    def dfs_helper(self, root, tofind):
         print(self.adj)
         stack = []
         number = 0
         path = []
         stack.insert(0, root)
+        found = False
         while stack:
             cur = stack.pop(0)
             path.append(cur)
+            if cur == tofind:
+                found = True
+                break
             number = number + 1
             for child in self.adj[cur]:
                 if child not in path:
                     stack.insert(0, child)
 
-        return number, path
+        return number, path, found
 
 if __name__ == "__main__":
     graph = Graph([[1,2,3],[3,4],[4],[4],[]])
-    nbr, path = graph.dfs(0)
+    nbr, path = graph.dfs(0, 2)
     print(path)
