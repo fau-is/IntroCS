c61cd34c77e405c84d81f460035e43b41f23568b
EinGartenzwerg <maximilian.harl@gmail.com>
2020-01-06 22:36:25 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,12 +15,13 @@ def simple_test():
     # check50.check()
     # check50.run("python3 dfs_gr_check.py").stdout("[0, 3, 4, 2]", regex=False).exit(0)
 
-    from re import match
-
-    expected = "[0, 3, 4, 2]"
-    actual = main()
-    if not match(expected, actual):
-        raise check50.Mismatch("hello, world\n", actual)
+    # from re import match
+    #
+    # expected = "[0, 3, 4, 2]"
+    # actual = check50
+    check50.run("python3 dfs_gr_check.py").stdout("[0, 3, 4, 2]", regex=False).exit(0)
+    # if not match(expected, actual):
+    #     raise check50.Mismatch("hello, world\n", actual)
 #     graph = Graph([[1,2,3],[3,4],[4],[4],[]])
 #     nbr, path = graph.dfs(0, 2)
 #     return path
