077a95c213e6ccd780b08363287cb835f3cf8835
EinGartenzwerg <maximilian.harl@gmail.com>
2020-01-06 22:38:59 +0200

    Update dfs_gr_check.py

--- dfs_gr_check.py
+++ dfs_gr_check.py
@@ -2,7 +2,7 @@ import sys
 from dfs_gr import Graph
 
 
-def main():
+if __name__ == '__main__':
     graph = Graph([[1,2,3],[3,4],[4],[4],[]])
     nbr, path = graph.dfs(0, 2)
     print(path, end="")
