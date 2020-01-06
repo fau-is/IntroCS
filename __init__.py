e03271ce56aec2c2529c8a215adece235d4a02e4
EinGartenzwerg <maximilian.harl@gmail.com>
2020-01-06 23:13:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ def exists():
     check50.include("bin_tree_check_2.py")
     
 
-@check50.check(compiles)
+@check50.check(exists)
 def test_ez():
     """example:
             30
@@ -24,14 +24,14 @@ def test_ez():
         .stdout("10, 20, 25, 30, 40, 50, 30, 20, 10, 25, 40, 50, 10, 25, 20, 50, 40, 30, "
                 , regex=False).exit(0)
 
-@check50.check(compiles)
+@check50.check(exists)
 def test_0_9():
     """test with insert 0-9"""
     check50.run("python3 bin_tree_check_1.py")\
         .stdout("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6, 3, 2, 1, 5, 4, 9, 8, 7, 10, 1, 2, 4, 5, 3, 7, 8, 10, 9, 6, "
                 , regex=False).exit(0)
     
-@check50.check(compiles)
+@check50.check(exists)
 def doubles():
     """test double values"""
     check50.run("python3 bin_tree_check_2.py")\
