d810d8a6c134ae37d2ba7671296d3d3f10342188
EinGartenzwerg <maximilian.harl@gmail.com>
2020-01-06 22:27:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,25 @@
 import check50
-import dfs_gr_check as dfsc
 
 @check50.check()
 def exists():
     """"runs"""
     check50.exists("dfs_gr.py")
+    check50.include("lookup.py")
+    check50.py.append_code("helpers.py", "lookup.py")
     
 
-# @check50.check()
-# def simple_test():
-#     """Simple Test"""
-#     check50.check()
-#     check50.run("python3 dfs_gr_check.py").stdout("[0, 3, 4, 2]", regex=False).exit(0)
-#
-#     from re import match
-#
-#     expected = "[0, 3, 4, 2]"
-#     actual = dfsc.main
-#     if not match(expected, actual):
-#         raise check50.Mismatch("hello, world\n", actual)
+@check50.check(exists)
+def simple_test():
+    """Simple Test"""
+    # check50.check()
+    # check50.run("python3 dfs_gr_check.py").stdout("[0, 3, 4, 2]", regex=False).exit(0)
+
+    from re import match
+
+    expected = "[0, 3, 4, 2]"
+    actual = main()
+    if not match(expected, actual):
+        raise check50.Mismatch("hello, world\n", actual)
+#     graph = Graph([[1,2,3],[3,4],[4],[4],[]])
+#     nbr, path = graph.dfs(0, 2)
+#     return path
