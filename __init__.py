ccfd326034dd556c55192f9229d21ca446666348
EinGartenzwerg <maximilian.harl@gmail.com>
2020-01-06 22:20:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,15 +8,15 @@ def exists():
     check50.exists("dfs_gr.py")
     
 
-@check50.check()
-def simple_test():
-    """Simple Test"""
-    check50.check()
-    check50.run("python3 dfs_gr_check.py").stdout("[0, 3, 4, 2]", regex=False).exit(0)
-
-    from re import match
-
-    expected = "[0, 3, 4, 2]"
-    actual = dfsc.main
-    if not match(expected, actual):
-        raise check50.Mismatch("hello, world\n", actual)
+# @check50.check()
+# def simple_test():
+#     """Simple Test"""
+#     check50.check()
+#     check50.run("python3 dfs_gr_check.py").stdout("[0, 3, 4, 2]", regex=False).exit(0)
+#
+#     from re import match
+#
+#     expected = "[0, 3, 4, 2]"
+#     actual = dfsc.main
+#     if not match(expected, actual):
+#         raise check50.Mismatch("hello, world\n", actual)
