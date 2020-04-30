c88b5ea90335cf177f31dff10fa7afcbf348b39d
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-04-30 22:48:25 +0200

    added Problem lower.c to Pset2

---
+++ lower.c
@@ -0,0 +1,16 @@
+/*Write a Program that can read a given Array of chars and convert capitalized letters into non capitalized letters
+ * and vice versa. Make sure that should you loop your program has an included function that always calculates the size
+ * of your array! The output for the given array should look as follows: "[ a, B, c, D, e, F ]"
+ * HINT: Try looking at the Ascii table and use functions included in the ctype.h library*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+char array[] =  { 'A', 'b', 'C', 'd', 'E', 'f' };
+
+int main(void)
+{
+    //ToDo
+    return 0;
+}
+
