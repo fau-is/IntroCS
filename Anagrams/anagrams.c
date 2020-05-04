d08c05ab3a3140111a73ab5acd93e22fd56dc863
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-04 16:24:12 +0200

    rm comments anagrams.c

--- Anagrams/anagrams.c
+++ Anagrams/anagrams.c
@@ -1,11 +1,3 @@
-/*Write a Program that can intake 2 strings i.e. words and check whether they are anagrams. I.e. 'army' & 'mary' or
- * 'bored' & 'robed'. The function doing the actual checking should be outside of your main program as in the code
- * we provide you with. Please note that you can add the <cs50.h> library at your free will for functions such as
- * get_string etc. However, this is not necessary! Other than that all the Libraries you need are included in the
- * Header already. Make sure that your check is case sensitive i.e. that your program recognizes 'ARMY' to be a
- * anagram for 'mary'.
-*/
-
 #include <stdio.h>
 #include <ctype.h>
 //#include <cs50.h>
