99b8af2cc6387a059d596003ddebfe92dff3f568
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-04 20:53:16 +0200

    added python checks to inverse.c

---
+++ Inverse/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists_inverse():
+    """inverse.c exists in WD."""
+    check50.exists("inverse.c")
+
+@check50.check(exists_inverse)
+def compiles_inverse():
+    """inverse.c compiles."""
+    check50.c.compile("inverse.c", lcs50=True)
+
+@check50.check(compiles_inverse)
+def inverse_stdin():
+    """print out inverse"""
+    check50.run("./inverse Supercalifragilistic").stdout("citsiligarfilacrepuS")
