0b07e288680a4669cfce8d6de097e8535b402501
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-04 16:17:58 +0200

    updated anagrams.c

--- Anagrams/anagrams.c
+++ Anagrams/anagrams.c
@@ -4,11 +4,7 @@
  * get_string etc. However, this is not necessary! Other than that all the Libraries you need are included in the
  * Header already. Make sure that your check is case sensitive i.e. that your program recognizes 'ARMY' to be a
  * anagram for 'mary'.
- * Hints:
- * 1. This is comparable to an evolution of a strcmpr function.
- * 2. Think of the Letters in the alphabet and how often one letter can appear in a word.
- * 3. Again the ASCII Table might be of help. */
-
+*/
 
 #include <stdio.h>
 #include <ctype.h>
