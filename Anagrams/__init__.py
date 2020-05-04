e565ec8155e5ad71482654e3efbb7ee2036cdb34
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-04 20:58:25 +0200

    added python checks to anagrams.c

---
+++ Anagrams/__init__.py
@@ -0,0 +1,18 @@
+import check50
+import check50.c
+
+
+@check50.check()
+def exists_anagrams():
+    """anagrams.c exists in WD."""
+    check50.exists("anagrams.c")
+
+@check50.check(exists_anagrams)
+def compiles_anagrams():
+    """anagrams.c compiles."""
+    check50.c.compile("anagrams.c", lcs50=True)
+
+@check50.check(compiles_anagrams)
+def anagram_me():
+    """Anagram me"""
+    check50.run("./lowupper").stdin("mary", "army").stdout("Strings are anagrams")
