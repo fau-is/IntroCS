e31c596d38220699028a731dbdcf3eefcc84a65b
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-04 15:47:25 +0200

    added Problem anagrams.c to Pset2

---
+++ anagrams.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <ctype.h>
+//#include <cs50.h>
+
+int anagram(char *a, char *b);
+const int N = 50;
+const int alphabet = 26;
+
+int main(void)
+{
+    char a[N], b[N];
+    printf("Enter 1st Word:");
+    gets(a);
+    printf("Enter 2nd Word:");
+    gets(b);
+
+    if (anagram(a, b))
+    {
+        printf("The words are anagrams\n");
+    }
+    else
+    {
+        printf("The words are not anagrams\n");
+    }
+    return 0;
+
+}
+
+int anagram(char *a, char *b)
+{
+    int _a[26] = {0}, _b[26] = {0};
+    int counter = 0;
+
+    while (a[counter] != '\0')
+    {
+        _a[tolower(a[counter]) - 97] ++;
+        printf("%i\n", _a[counter]);
+        counter++;
+    }
+
+    counter = 0;
+    while (b[counter] != '\0')
+    {
+        _b[tolower(b[counter]) - 97] ++;
+        counter++;
+    }
+
+    for (int i = 0; i < alphabet; i++)
+    {
+        if(_a[i] != _b[i])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
