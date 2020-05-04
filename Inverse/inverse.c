9879ee217b2a2e4b8b9c098be9d3e56b8916d8e7
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-04 16:24:40 +0200

    rm comments inverse.c

--- Inverse/inverse.c
+++ Inverse/inverse.c
@@ -1,9 +1,3 @@
-/* Write a program that takes a command line input only allowing 1 Word. Use this command line input and make sure that the input
-only consists of letters in the alphabet. Write your code to implement your alphabet check as a function outside of
-your main Program. In your Program invert the input and print it out.
-Example: Input = Hello; Output = olleH
-Hint: Use isalpha()*/
-
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
