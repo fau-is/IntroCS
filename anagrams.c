b8dee9daca8a5fe381aa68e4fdacd5bd45325754
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-05 11:35:55 +0200

    anagrams.c added corrections

--- anagrams.c
+++ anagrams.c
@@ -12,11 +12,11 @@ int main(void)
 
     if (anagram(a, b))
     {
-        printf("The words are anagrams\n");
+        printf("Strings are anagrams\n");
     }
     else
     {
-        printf("The words are not anagrams\n");
+        printf("Strings are not anagrams\n");
     }
     return 0;
 
