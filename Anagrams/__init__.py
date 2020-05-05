b23dc864f4e8ad61bc6ca8b5fd38f19d4ca81fdd
ho87nyze <kolo93tosO>
2020-05-05 12:00:10 +0200

    updated README.md anagram.c

--- Anagrams/__init__.py
+++ Anagrams/__init__.py
@@ -7,12 +7,26 @@ def exists_anagrams():
     """anagrams.c exists in WD."""
     check50.exists("anagrams.c")
 
+
 @check50.check(exists_anagrams)
 def compiles_anagrams():
     """anagrams.c compiles."""
     check50.c.compile("anagrams.c", lcs50=True)
 
+
 @check50.check(compiles_anagrams)
-def anagram_me():
-    """Anagram me"""
-    check50.run("./lowupper").stdin("mary", "army").stdout("Strings are anagrams").run("./lowupper").stdin("mary", "arny").stdout("Strings are not anagrams")
+def anagram_true():
+    """It's an anagram"""
+    check50.run("./anagrams").stdin("mary", "army").stdout("Strings are anagrams")
+
+
+@check50.check(compiles_anagrams)
+def anagram_true_caseInsensitive():
+    """It's an anagram"""
+    check50.run("./anagrams").stdin("mary", "ARMY").stdout("Strings are anagrams")
+
+
+@check50.check(compiles_anagrams)
+def anagram_false():
+    """Not an anagram"""
+    check50.run("./anagrams").stdin("mary", "arny").stdout("Strings are not anagrams")
