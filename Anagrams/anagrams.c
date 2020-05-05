914683bc612149fb0f7a80eb83efaf5e62a648b0
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-05 08:58:33 +0200

    updated anagrams.c

--- Anagrams/anagrams.c
+++ Anagrams/anagrams.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
-//#include <cs50.h>
+#include <cs50.h>
 
 //this is the initialisation of your checker function
 int anagram(char *a, char *b);
-//As a help we have initialized a Constant for your strings
-const int N = 50;
 //This should be a hint in itself ;)
 const int alphabet = 26;
 
 int main(void)
 {
     //We have initialized your strings for you already
-    char a[N], b[N];
+    char *a;
+    char *b;
     //ToDo
     return 0;
 }
