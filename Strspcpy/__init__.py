066a9ec48538731155a82ce83aca4cba725590f5
ho87nyze <kolo93tosO>
2020-05-06 14:35:22 +0200

    update __init__.py

--- Strspcpy/__init__.py
+++ Strspcpy/__init__.py
@@ -8,19 +8,19 @@ def compiles():
     check50.c.compile("strspcpy.c")
 
 
-@check50.check(compiles_lowupper)
+@check50.check(compiles)
 def check_copy():
     """Can print 3 to 10 characters repetitively"""
     check50.run("./strspcpy 10 abc").stdout("abcabcabca\n")
 
 
-@check50.check(compiles_lowupper)
+@check50.check(compiles)
 def check_copy_small():
     """Can copy a large string into a small output"""
     check50.run("./strspcpy 1 abc").stdout("a\n")
 
 
-@check50.check(compiles_lowupper)
+@check50.check(compiles)
 def check_copy_same_size():
     """Can replicate the string exactly"""
     check50.run("./strspcpy 3 abc").stdout("abc\n")
