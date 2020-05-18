4b7eaeba1f4d46ab3017196a521da66943553240
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-18 17:07:40 +0200

    created checks for pset3

---
+++ Copycat/__init__.py
@@ -0,0 +1,24 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """copycat.c exists."""
+    check50.include("copy.txt")
+    check50.include("original.txt")
+    check50.exists("copycat.c")
+
+@check50.check(exists)
+def compiles():
+    """copycat.c compiles."""
+    check50.c.compile("copycat.c", lcs50=True)
+
+@check50.check(compiles)
+def first_line():
+    """first line is correct"""
+    check50.run("./copycat").exit(0, timeout=10)
+    with open("original.txt") as file1, open("copy.txt") as file2:
+        for line1, line2 in zip(file1, file2):
+            if line1 != line2:
+                raise check50.Failure("texts do not match")
+
---
+++ Witchcraft/__init__.py
@@ -0,0 +1,32 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """copycat.c exists."""
+    check50.include("myletter.txt")
+    check50.include("hopwarts.txt")
+    check50.exists("wizardry.c")
+
+@check50.check(exists)
+def compiles():
+    """copycat.c compiles."""
+    check50.c.compile("wizardry.c", lcs50=True)
+
+@check50.check(compiles)
+def argc():
+    """takes three command arguments"""
+    check50.run("./wizardry hopwarts").stdout("./wizardry infile outfile name")
+    check50.run("./wizardry").stdout("./wizardry infile outfile name")
+    check50.run("./wizardry hopwarts myletter").stdout("./wizardry infile outfile name")
+    check50.run("./wizardry hopwarts myletter Name1 Name2").stdout("./wizardry infile outfile name")
+
+@check50.check(argc)
+def matches():
+    """You created your own letter of Acceptance"""
+    check50.run("./wizardry hopwarts.txt myletter.txt Goehl").exit(0, timeout=10)
+    with open("hopwarts.txt") as file1, open("myletter.txt") as file2:
+        for line1, line2 in zip(file1, file2):
+            if line1 != line2:
+                raise check50.Failure("texts do not match")
+
