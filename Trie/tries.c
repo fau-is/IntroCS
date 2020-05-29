c3010d6e47ba400b25c19ebdb051e002afe15777
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-05-29 17:10:18 +0200

    updated Pset4

--- Trie/tries.c
+++ Trie/tries.c
@@ -23,59 +23,18 @@ int count = 0;
 
 unsigned int counting(node *temp)
 {
-    if(temp->is_city == true)
-    {
-        count++;
-    }
-    for(int i = 0; i < N; i++)
-    {
-        if(temp->children[i] != NULL)
-        {
-            counting(temp->children[i]);
-        }
-    }
+    //ToDo
     return count;
 }
 
 bool check(const char *word)
 {
-    node *pointer = root;
-    for (int j = 0; j <= strlen(word); j++)
-    {
-        int index = tolower(word[j]) - 'a';
-        if (word[j] != '\0')
-        {
-            if (pointer -> children[index] == NULL)
-            {
-                return false;
-            }
-            else
-            {
-                pointer = pointer -> children[index];
-            }
-
-        }
-    }
-    if (pointer -> is_city == true)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    //ToDo
 }
 
 void unload(node *temp)
 {
-    for(int i = 0; i < N; i++)
-    {
-        if(temp->children[i] != NULL)
-        {
-            unload(temp->children[i]);
-        }
-    }
-    free(temp);
+    //ToDo
 }
 
 int main(void)
@@ -101,31 +60,8 @@ int main(void)
 
     char word[LENGTH + 1];
 
-    while (fscanf(file, "%s", word) != EOF)
-    {
-        int length = strlen(word);
-        node *current = root;
-        printf("%s\n", word);
-        for(int i = 0; i < length; i++)
-        {
-            int index = tolower(word[i]) - 'a';
-            if (current -> children[index] == NULL)
-            {
-                node *pNode = malloc(sizeof(node));
-                if (pNode == NULL)
-                {
-                    return false;
-                }
-                current->children[index] = pNode;
-                current = pNode;
-            }
-            else
-            {
-                current = current->children[index];
-            }
-        }
-        current -> is_city = true;
-    }
+    //ToDo
+
     node *temp = root;
     printf("Cities in Trie: %i\n", counting(temp));
     for (int i = 0; i < size; i++)
