c22916ca3f294c8e78bdbbee7cb216f391b32d29
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-06-15 12:48:41 +0200

    added gitignore
    Signed-off-by: christophergoehl <56957100+christophergoehl@users.noreply.github.com>

---
+++ Clean/clean.py
@@ -0,0 +1,21 @@
+import string
+
+def main():
+
+    file = open('silent_sea.txt', 'r')
+    content = file.read()
+    file.close()
+    L = []
+    for ch in content:
+        if ch not in string.punctuation:
+            L.append(ch)
+        else:
+            L.append('')
+    cleaned = ''.join(L)
+
+    file = open('silent_sea_clean.txt', 'w')
+    file.write(cleaned)
+    file.close()
+
+if __name__== "__main__":
+    main()
