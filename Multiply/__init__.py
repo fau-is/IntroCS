96556aca10f550dd31b8fcdad88144b1c6e40e85
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-06-16 17:25:06 +0200

    submit50 pset4

--- Multiply/__init__.py
+++ Multiply/__init__.py
@@ -5,12 +5,17 @@ import check50.c
 
 @check50.check()
 def exists_multiply():
-    """mario-2.c exists in WD."""
+    """multiply.c exists in WD."""
     check50.exists("multiply.c")
 
-@check50.check(multiply)
-def compiles_mario_2():
-    """mario-2.c compiles."""
+@check50.check(exists_multiply)
+def compiles():
+    """multiply compiles."""
     check50.c.compile("multiply", lcs50=True)
+
+@check50.check(compiles)
+def multiplies():
+    """multiply does what it is supposed to do."""
+    check50.run("./multiply").stdout("[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]")
 
 
