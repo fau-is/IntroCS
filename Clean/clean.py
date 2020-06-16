e31f6f81ca6bb24eafb85a252384f53fc2108b9c
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-06-16 16:48:58 +0200

    blabla
    Signed-off-by: christophergoehl <56957100+christophergoehl@users.noreply.github.com>

--- Clean/clean.py
+++ Clean/clean.py
@@ -2,20 +2,7 @@ import string
 
 def main():
 
-    file = open('silent_sea.txt', 'r')
-    content = file.read()
-    file.close()
-    L = []
-    for ch in content:
-        if ch not in string.punctuation:
-            L.append(ch)
-        else:
-            L.append('')
-    cleaned = ''.join(L)
-
-    file = open('silent_sea_clean.txt', 'w')
-    file.write(cleaned)
-    file.close()
+    #ToDo
 
 if __name__== "__main__":
     main()
