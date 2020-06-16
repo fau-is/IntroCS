b8728cc398fdbb9b55889dbe6e5ad963695c388a
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-06-16 16:49:15 +0200

    blabla
    Signed-off-by: christophergoehl <56957100+christophergoehl@users.noreply.github.com>

--- Time/time.py
+++ Time/time.py
@@ -3,19 +3,9 @@ import pytz
 
 A = ['Europe/Berlin', 'America/New_York', 'Europe/London', 'Asia/Shanghai', "Africa/Accra"]
 
-
 def main():
-    t_format = "%d/%m/%Y, %H:%M:%S"
-    now = datetime.now()
-    now = now.replace(tzinfo=pytz.utc)
 
-    local = datetime.now()
-    print("Local:", local.strftime(t_format))
-
-    for cities in A:
-        t = now.astimezone(pytz.timezone(cities))
-        print(cities, ":", t.strftime(t_format))
-
+    #ToDo
 
 if __name__ == "__main__":
     main()
