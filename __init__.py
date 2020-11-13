2e82187835453c3dd7a0863c3d0a6f30430f4c34
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-11-13 12:12:42 +0100

    OOP_MIT upload
    Signed-off-by: christophergoehl <56957100+christophergoehl@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -3,8 +3,10 @@
 import unittest
 from ps5 import *
 from datetime import timedelta
+import check50
+import check50.py
 
-
+@check50.check()
 class ProblemSet5NewsStory(unittest.TestCase):
     def setUp(self):
         pass
@@ -37,6 +39,7 @@ class ProblemSet5NewsStory(unittest.TestCase):
                           'test description', 'test link', datetime.now())
         self.assertEqual(type(story.get_pubdate()), datetime)
 
+@check50.check()
 class ProblemSet5(unittest.TestCase):
     def setUp(self):
         class TrueTrigger:
@@ -285,7 +288,7 @@ class ProblemSet5(unittest.TestCase):
         self.assertTrue(nob in filtered_stories)
         self.assertEqual(2, len(filtered_stories))
 
-
+@check50.check()
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(ProblemSet5NewsStory))
