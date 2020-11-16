2c5adf672269e80387fab33e95fb510af590ce43
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-11-16 12:39:37 +0100

    updated __init__.py
    Signed-off-by: christophergoehl <56957100+christophergoehl@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -6,80 +6,80 @@ from ps5_test import *
 @check50.check()
 def testNewsStoryConstructor():
     """testNewsStoryConstructor"""
-    check50.run(python -m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryConstructor)
+    check50.run("python -m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryConstructor")
 
 @check50.check()
 def testNewsStoryGetGuid():
     """testNewsStoryGetGuid"""
-    check50.run(python -m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetGuid)
+    check50.run("python -m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetGuid")
 
 @check50.check()
 def testNewsStoryGetTitle():
     """testNewsStoryGetGuid"""
-    check50.run(python -m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetTitle)
+    check50.run("python -m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetTitle")
 
 @check50.check()
 def testNewsStoryGetdescription():
     """testNewsStoryGetdescription"""
-    check50.run(python - m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetdescription)
+    check50.run("python - m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetdescription")
 
 @check50.check()
 def testNewsStoryGetLink():
     """testNewsStoryGetLink"""
-    check50.run(python - m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetLink)
+    check50.run("python - m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetLink")
 
 @check50.check()
 def testNewsStoryGetTime():
     """testNewsStoryGetTime"""
-    check50.run(python - m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetTime)
+    check50.run("python - m unittest ps5_test.ProblemSet5NewsStory.testNewsStoryGetTime")
 
 @check50.check()
 def test1TitleTrigger():
     """test1TitleTrigger"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test1TitleTrigger)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test1TitleTrigger")
 
 @check50.check()
 def test2TitleTrigger():
     """test2TittleTrigger"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test2TitleTrigger)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test2TitleTrigger")
 
 @check50.check()
 def test2DescriptionTrigger():
     """test2DescriptionTrigger"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test2DescriptionTrigger)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test2DescriptionTrigger")
 
 @check50.check()
 def test3altBeforeAndAfterTrigger():
     """test3altBeforeAndAfterTrigger"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test3altBeforeAndAfterTrigger)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test3altBeforeAndAfterTrigger")
 
 @check50.check()
 def test3BeforeAndAfterTrigger():
     """test3BeforeAndAfterTrigger"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test3BeforeAndAfterTrigger)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test3BeforeAndAfterTrigger")
 
 @check50.check()
 def test4NotTrigger():
     """test4NotTrigger"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test4NotTrigger)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test4NotTrigger")
 
 @check50.check()
 def test5AndTrigger():
     """test5AndTrigger"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test5AndTrigger)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test5AndTrigger")
 
 @check50.check()
 def test6OrTrigger():
     """test6OrTrigger"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test6OrTrigger)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test6OrTrigger")
 
 @check50.check()
 def test7FilterStories():
     """test7FilterStories"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test7FilterStories)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test7FilterStories")
 
 @check50.check()
 def test8FilterStories2():
     """Test8FilterStories2"""
-    check50.run(python - m unittest ps5_test.ProblemSet5.test8FilterStories2)
+    check50.run("python - m unittest ps5_test.ProblemSet5.test8FilterStories2")
 
