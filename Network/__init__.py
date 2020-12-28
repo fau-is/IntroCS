f57089bd5927f411eea379876fd316735d5be44c
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-12-28 09:16:59 +0100

    updated check50
    Signed-off-by: christophergoehl <56957100+christophergoehl@users.noreply.github.com>

--- Network/__init__.py
+++ Network/__init__.py
@@ -14,7 +14,7 @@ def exists():
 @check50.check(exists)
 def add_vertex():
     """adds vertex"""
-    n = network.Graph
-    add_vertex(n, 'A')
+    n = network.Graph()
+    n.add_vertex('A')
     if 'A' not in n:
-        raise check50.Failure()
+        raise check50.Failure("Expected add Vertex")
