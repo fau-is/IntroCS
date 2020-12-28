af07f0bd23b7b92ed37eb2e0c0e870ebb05c4cea
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-12-28 10:17:34 +0100

    updated check50
    Signed-off-by: christophergoehl <56957100+christophergoehl@users.noreply.github.com>

--- Network/__init__.py
+++ Network/__init__.py
@@ -18,3 +18,27 @@ def add_vertex():
     n.add_vertex('A')
     if 'A' not in n:
         raise check50.Failure("Expected add Vertex")
+
+@check50.check(add_vertex)
+def add_edge():
+    """adds weighted edges"""
+    Vertices = ['A', 'B', 'C', 'D']
+    Edges = ['AB2', 'AC3', 'BD2', 'CB4']
+    n = network.Graph()
+    for vertex in Vertices:
+        n.add_vertex(vertex)
+    for edge in Edges:
+        n.add_edge(edge[:1], edge[1], int(edge[2:]))
+
+    #Edges outgoing A
+    if ('B', 2) not in n['A'] and ('C', 3) not in n['A']:
+        raise check50.Failure("Edges A->B w:2 and A->C w:3 (in requested format)", n['A'])
+    #Edges outgoing B
+    if ('A', 2) not in n['B'] and ('D', 2) not in n['B'] and ('C', 4) not in n['B']:
+        raise check50.Failure("Edges B->A w:2; B->D w:2 and B->C w:4 (in requested format)", n['B'])
+    #Edges outgoing C
+    if ('A', 3) in n['C'] and ('B', 4) in n['C']:
+        raise check50.Failure("Edges C->A w:3 and C->B w:4 (in requested format)", n['C'])
+    #Edges outgoing D
+    if ('B', 2) in n['D']:
+        raise check50.Mismatch("Edge D->B w:2 (in requested format)", n['D'])
