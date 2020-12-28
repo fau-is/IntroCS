9a6e25881f78fe3cc0aa43750212678d7107481d
christophergoehl <56957100+christophergoehl@users.noreply.github.com>
2020-12-28 13:44:01 +0100

    updated check50
    Signed-off-by: christophergoehl <56957100+christophergoehl@users.noreply.github.com>

--- Network/main.py
+++ Network/main.py
@@ -15,14 +15,18 @@ def print_graph(g):
     os.remove('network')
 
 g = Graph()
-edges = ['AB3', 'AE2', 'AF2', 'BA3', 'BE3', 'BF5', 'BD1', 'BC5', 'CB5', \
-         'CD1', 'DC1', 'DB1', 'DE1', 'EB3', 'EA2', 'EF1', 'FA2', 'FB5', 'FE1', 'ZY1', 'YX3']
-for edge in edges:
-    g.add_edge(edge[:1], edge[1], int(edge[2:]))
+Edges = [('yahoo.com', 'google.com', 3), ('yahoo.com', 'facebook.com', 2), ('yahoo.com', 'twitter.com', 2), ('google.com', 'facebook.com', 5), ('google.com', 'twitter.com', 3), \
+        ('google.com', 'instagram.com', 1), ('google.com', 'reddit.com', 5), ('LordVoldemodem', 'google.com', 1), ('reddit.com', 'instagram.com', 1), ('instagram.com', 'twitter.com', 1), \
+        ('twitter.com', 'facebook.com', 1), ('DesktopD', 'DesktopC', 3), ('DesktopD', 'Mobile1', 1), ('Mobile1', 'DesktopC', 2), ('DesktopC', 'Modem', 1)]
+
+for edge in Edges:
+    a, b, c = edge
+    g.add_edge(a, b, c)
 
 print(g)
-print(g.dfs('A'))
-print(g.bfs('A'))
-g.dijkstra('A', 'D')
-print(g.bfs('Y'))
-print_graph(g)
+print(g.dfs('yahoo.com'))
+print(g.bfs('twitter.com'))
+a, b = g.dijkstra('yahoo.com', 'reddit.com')
+print('Shortest distance is ' + str(a))
+print('Path: ' + str(b))
+print(g.bfs('Mobile1'))
