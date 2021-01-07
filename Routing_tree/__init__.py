bc5f80777357f83f8b444c51aab8eb36eeb3ad4c
ho87nyze <kolo93tosO>
2021-01-07 13:46:02 +0100

    Deleted check.py, Adjusted everything else

--- Routing_tree/__init__.py
+++ Routing_tree/__init__.py
@@ -1,10 +1,15 @@
 import check50
 import check50.py
 
-#check50 fau-is/IntroCS/PyGraphsTrees/Routing_tree --local
-
 check50.include('routing_tree.py')
 routing_tree = check50.py.import_('routing_tree.py')
+
+domains1 = [("computer.com", '0.0.0.0'), ("array.com", '1.0.0.0'), ("binary.com", '0.1.0.0'),
+            ("ecommerce.com", '0.0.1.0'), ("data.com", '0.0.0.1')]
+domains2 = [("computer.com", '0.0.0.0'), ("array.com", '1.0.0.0'), ("binary.com", '0.1.0.0'),
+            ('hardware.com', '2.0.0.0'), ("ecommerce.com", '0.0.1.0'), ('gigabyte.com', '1.1.1.1'),
+            ("data.com", '0.0.0.1'), ('interconnectivity.com', '0.2.0.0')]
+
 
 @check50.check()
 def exists():
@@ -13,10 +18,18 @@ def exists():
 
 
 @check50.check(exists)
+def compiles():
+    """Routing_tree.py has no syntax errors"""
+    check50.py.compile("routing_tree.py")
+
+
+@check50.check(compiles)
 def add_root():
-    """adds a root"""
+    """BST can add a single domain"""
     routing_tree.BST.add('Fantastic.com', '1.1.1.1')
     n = routing_tree.BST.root
+    if n is None:
+        raise check50.Failure("Root was None")
     if n.domain != 'Fantastic.com':
         raise check50.Mismatch('Fantastic.com', n.domain)
 
@@ -24,138 +37,129 @@ def add_root():
         raise check50.Mismatch('1.1.1.1', n.IP)
 
 
-@check50.check(add_root)
+@check50.check(compiles)
 def add_tree_child():
-    """adds children"""
-    BST = [("sebastian.com", '0.0.0.0'), ("sebastion.com", '1.0.0.0'), ("sabastian.com", '0.1.0.0'),
-           ("sebastimon.com", '0.0.1.0')]
-    for domain, ip in BST:
+    """BST can add several domains"""
+    # Build Tree
+    for domain, ip in domains1:
         routing_tree.BST.add(domain, ip)
+    # Root shortcut
     n = routing_tree.BST.root
-    if n is None:
-        raise check50.Failure()
-    if n.domain != 'sebastian.com':
-        raise check50.Mismatch('sebastian.com', n.domain)
 
+    # Check root
+    if n is None:
+        raise check50.Failure("Root was None")
+    if n.domain != 'computer.com':
+        raise check50.Mismatch('sebastian.com', n.domain)
     if n.IP != '0.0.0.0':
         raise check50.Mismatch('0.0.0.0', n.IP)
 
-    n = n.left
-    if n is None:
-        raise check50.Failure()
-    if n.domain != 'sabastian.com':
-        raise check50.Mismatch('sabastian.com', n.domain)
+    # Check left child of root
+    if n.left is None:
+        raise check50.Failure("Left child of root was none")
+    if n.left.domain != 'array.com':
+        raise check50.Mismatch('array.com', n.left.domain)
+    if n.left.IP != '1.0.0.0':
+        raise check50.Mismatch('1.0.0.0', n.left.IP)
 
-    if n.IP != '0.1.0.0':
-        raise check50.Mismatch('0.1.0.0', n.IP)
+    # Check right child of root
+    if n.right is None:
+        raise check50.Failure("Right child of root was None")
+    if n.right.domain != 'ecommerce.com':
+        raise check50.Mismatch('ecommerce.com', n.right.domain)
+    if n.right.IP != '0.0.1.0':
+        raise check50.Mismatch('0.0.1.0', n.right.IP)
 
-    n = routing_tree.BST.root.right
-    if n is None:
-        raise check50.Failure()
-
-    if n.domain != 'sebastion.com':
-        raise check50.Mismatch('sebastion.com', n.domain)
-
-    if n.IP != '1.0.0.0':
-        raise check50.Mismatch('1.0.0.0', n.IP)
-
-    n = routing_tree.BST.root.right.left
-    if n is None:
-        raise check50.Failure()
-
-    if n.domain != 'sebastimon.com':
-        raise check50.Mismatch('sebastimon.com', n.domain)
-
-    if n.IP != '0.0.1.0':
-        raise check50.Mismatch('0.0.1.0', n.IP)
+    # Check root -> right -> left
+    if n.right.left is None:
+        raise check50.Failure('Child at root -> right -> left was None')
+    if n.right.left.domain != 'data.com':
+        raise check50.Mismatch('data.com', n.right.left.domain)
+    if n.right.left.IP != '0.0.0.1':
+        raise check50.Mismatch('0.0.0.1', n.right.left.IP)
 
 
 @check50.check(add_tree_child)
 def find_node():
-    """finds node"""
-    BST = [("computer.com", '0.0.0.0'), ("array.com", '1.0.0.0'), ("binary.com", '0.1.0.0'),
-           ("ecommerce.com", '0.0.1.0'), ("data.com", '0.0.0.1')]
-    for domain, ip in BST:
+    """BST can locate a domain and a BST node stores the correct IP address"""
+    for domain, ip in domains1:
         routing_tree.BST.add(domain, ip)
 
-    if routing_tree.BST.find("binary.com") is None:
-        raise check50.Failure("Did not expect None-type")
-    if routing_tree.BST.find("binary.com") is False:
-        raise check50.Failure("Did not expect bool=False")
     if routing_tree.BST.find("binary.com"):
         n = routing_tree.BST.find("binary.com")
         if n.domain != "binary.com":
-            raise check50.Mismatch("binary.com", n.domain)
+            raise check50.Mismatch("binary.com", n.domain, "Find located the wrong vertex")
+        if n.ip != "0.1.0.0":
+            raise check50.Mismatch("0.1.0.0", n.ip, "It seems that the IP-Address is not stored correctly")
+    else:
+        raise check50.Failure("Could not locate a domain which should exist")
 
-@check50.check(add_tree_child)
-def reject_find_node():
+
+@check50.check(find_node)
+def find_non_existent_node():
     """returns false if node not in tree"""
-    BST = [("computer.com", '0.0.0.0'), ("array.com", '1.0.0.0'), ("binary.com", '0.1.0.0'),
-           ("ecommerce.com", '0.0.1.0'), ("data.com", '0.0.0.1')]
-    for domain, ip in BST:
+    for domain, ip in domains1:
         routing_tree.BST.add(domain, ip)
 
     if routing_tree.BST.find("nonsense.com") is not False:
-        raise check50.Failure("Expected false")
-    if routing_tree.BST.find("binary.com") is False:
-        raise check50.Failure("Should not return bool=False")
+        raise check50.Failure("Expected False")
 
 
 @check50.check(add_tree_child)
-def bfs():
-    """Breadth First Search is correct"""
-    BFS = ['computer.com', 'array.com', 'hardware.com', 'binary.com', 'ecommerce.com', 'interconnectivity.com', 'data.com', 'gigabyte.com']
-    BST = [("computer.com", '0.0.0.0'), ("array.com", '1.0.0.0'), ("binary.com", '0.1.0.0'), ('hardware.com', '2.0.0.0'), ("ecommerce.com", '0.0.1.0'), ('gigabyte.com', '1.1.1.1'), ("data.com", '0.0.0.1'), ('interconnectivity.com', '0.2.0.0')]
-    for domain, ip in BST:
+def check_bfs():
+    """Checks whether output is generated in BFS manner"""
+    bfs = ['computer.com', 'array.com', 'hardware.com', 'binary.com', 'ecommerce.com', 'interconnectivity.com',
+           'data.com', 'gigabyte.com']
+    for domain, ip in domains2:
         routing_tree.BST.add(domain, ip)
-    if routing_tree.BST.bfs() != BFS:
-        raise check50.Mismatch(BFS, routing_tree.BST.bfs())
+
+    if routing_tree.BST.bfs() != bfs:
+        raise check50.Mismatch(bfs, routing_tree.BST.bfs())
 
 
 @check50.check(add_tree_child)
 def preorder_dfs():
-    """pre-order DFS is correct"""
-    DFS = ['array.com', 'binary.com', 'computer.com', 'data.com', 'ecommerce.com', 'gigabyte.com', 'hardware.com', 'interconnectivity.com']
-    BST = [("computer.com", '0.0.0.0'), ("array.com", '1.0.0.0'), ("binary.com", '0.1.0.0'), ('hardware.com', '2.0.0.0'), ("ecommerce.com", '0.0.1.0'), ('gigabyte.com', '1.1.1.1'), ("data.com", '0.0.0.1'), ('interconnectivity.com', '0.2.0.0')]
-    for domain, ip in BST:
+    """Checks whether output is in preorder-order using DFS"""
+    dfs = ['computer.com', 'array.com', 'binary.com', 'hardware.com', 'ecommerce.com', 'data.com', 'gigabyte.com',
+           'interconnectivity.com']
+    for domain, ip in domains2:
         routing_tree.BST.add(domain, ip)
-    if routing_tree.BST.preorder() != DFS:
-        raise check50.Mismatch(DFS, routing_tree.BST.preorder())
+
+    if routing_tree.BST.preorder() != dfs:
+        raise check50.Mismatch(dfs, routing_tree.BST.preorder())
+
 
 @check50.check(find_node)
 def delete():
     """Deletes Node"""
-    BST = BST = [("computer.com", '0.0.0.0'), ("array.com", '1.0.0.0'), ("binary.com", '0.1.0.0'), ('hardware.com', '2.0.0.0'), ("ecommerce.com", '0.0.1.0'), ('gigabyte.com', '1.1.1.1'), ("data.com", '0.0.0.1'), ('interconnectivity.com', '0.2.0.0')]
-    for domain, ip in BST:
+    for domain, ip in domains2:
         routing_tree.BST.add(domain, ip)
     n = routing_tree.BST.root
     if n is None:
         raise check50.Failure("No Root")
 
-    #1. Case one child
-    routing_tree.BST.deleteBST(routing_tree.BST.root, "array.com")
+    # 1. Case one child
+    routing_tree.BST.delete_method("array.com")
     if routing_tree.BST.find("array.com") is not False:
         raise check50.Failure("Expected deletion of array.com with one child did not occur")
     if routing_tree.BST.root.left.domain == 'array.com':
         raise check50.Failure("Deletion unsuccessful")
 
-    #2. Case no children
-    routing_tree.BST.deleteBST(routing_tree.BST.root, "data.com")
+    # 2. Case no children
+    routing_tree.BST.delete_method("data.com")
     if routing_tree.BST.find("data.com") is not False:
         raise check50.Failure("Expected deletion of data.com with no children did not occur")
     if routing_tree.BST.root.right.left.left is not None:
         raise check50.Failure("Deletion unsuccessful")
 
-    #3. Case two children
-    routing_tree.BST.deleteBST(routing_tree.BST.root, "hardware.com")
+    # 3. Case two children
+    routing_tree.BST.delete_method("hardware.com")
     if routing_tree.BST.find("hardware.com") is not False:
         raise check50.Failure("Expected deletion of data.com with no children did not occur")
     if routing_tree.BST.root.right.domain == 'hardware.com':
         raise check50.Failure("Deletion unsuccessful")
 
-    #4. Case deletes Root
-    routing_tree.BST.deleteBST(routing_tree.BST.root, routing_tree.BST.root.domain)
+    # 4. Case deletes Root
+    routing_tree.BST.delete_method(routing_tree.BST.root.domain)
     if routing_tree.BST.root.domain == "computer.com":
         raise check50.Failure("Root Deletion unsuccessful")
-
-
--- Routing_tree/dns.py
+++ Routing_tree/dns.py
@@ -1,43 +1,41 @@
 import csv
-from routing_tree import BST as Tree
+from routing_tree import BST
+
 
 def read_csv(file, do_ips):
-    f = open(file)
-    reader = csv.DictReader(f)
-    fields = reader.fieldnames
-    if "Domain" not in fields or "IP" not in fields:
-        exit("File must have Domain and IP columns")
-    for row in reader:
-        do_ips.append((row['Domain'], row['IP']))
-    f.close()
+    with open(file) as f:
+        reader = csv.DictReader(f)
+        fields = reader.fieldnames
+        if "Domain" not in fields or "IP" not in fields:
+            exit("File must have Domain and IP columns")
+        for row in reader:
+            do_ips.append((row['Domain'], row['IP']))
+
 
 def main():
     do_ips = []
     read_csv('do_ip.csv', do_ips)
     for domain, ip in do_ips:
-        Tree.add(domain, ip)
+        BST.add(domain, ip)
 
-    print('DFS: ', Tree.preorder())
-    print('BFS: ', Tree.bfs())
+    print('DFS (preorder): ', BST.preorder())
+    print('BFS: ', BST.bfs())
     print('Attempt to find facebook.com & google.com')
-    to_find = ["facebook.com", "google.com"]
-    for items in to_find:
-        vertex = Tree.find(items)
+    requests = ["facebook.com", "google.com"]
+    for domain in requests:
+        vertex = BST.find(domain)
         if vertex is not False:
-            print(vertex.domain, "can be found at", vertex.IP)
+            print(f"For {vertex.domain} go to the address: {vertex.IP}")
         else:
-            print(items, "does not exist")
-    print("Attempt to delete 'quelle.de'")
-    Tree.delete_method("quelle.de")
-    vertex = Tree.find("quelle.de")
+            print(f"Is there a typo in {domain}\t DNS_PROBE_FINISHED_NXDOMAIN")
+    print("Attempt to delete 'quelle.de' from DNS")
+    BST.delete_method("quelle.de")
+    vertex = BST.find("quelle.de")
     if vertex is not False:
-        print(vertex.domain, "can be found at", vertex.IP)
+        print("Unsuccessful deletion.")
     else:
-        print("quelle.de does not exist")
+        print("Deleted successfully!")
 
-    print('DFS: ', Tree.preorder())
-
-    print('BFS:', Tree.bfs())
 
 if __name__ == '__main__':
     main()
