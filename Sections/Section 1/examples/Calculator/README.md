308b915c4638da75f03fbf4e499aacab602706c5
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-01 21:07:31 +0000

    Hello, Sections

---
+++ Sections/Section 1/examples/Calculator/README.md
@@ -0,0 +1,22 @@
+# Let's have a look at a very basic program: A calculator
+
+## Intro
+If we combine loops, functions and getting input from users, we can create a first program.
+For now, let's not argue about usefulness ;).
+
+## Functionality
+In every loop, the program will ask which calculator operation should be executed. Depending on a user's input a function is called:
+- sum
+- modulo
+- multiply
+- division
+
+After performing the calculation the result is printed.
+
+{%next %}
+
+## File outline
+### Includes
+- <stdio.h>
+  - Required to print results to the command line
+  -
---
+++ Sections/Section 1/examples/Calculator/calculator.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// Functions
+void print_result(char c, int a, int b, int d);
+int sum(int a, int b);
+int modulo(int a, int b);
+int multiply(int a, int b);
+int division(int a, int b);
+void mean();
+int quit();
+
+int main(void)
+{
+    char action;
+
+    while(true)
+    {
+        action = get_char("What do you want to do? \n\ta(ddition) \n\ts(ubstraction) \n\tp(roduct) \n\td(ivision) \n\tr(emainder)\n\tm(ean)\n\tq(uit)\nAction: ");
+
+        if(action == 'q')
+        {
+            return 0;
+        }
+
+        if (action == 'm')
+        {
+            mean();
+            continue;
+        }
+
+        int a = get_int("a = ");
+        int b = get_int("b = ");
+
+        if (action == 'a')
+        {
+            print_result('+', a, b, a + b);
+        }
+        else if(action == 's')
+        {
+            print_result('-', a, b, a - b);
+        }
+        else if(action == 'p')
+        {
+            print_result('*', a, b, a * b);
+        }
+        else if(action == 'd')
+        {
+            print_result('/', a, b, division(a, b));
+        }
+        else if(action == 'r')
+        {
+            print_result('%', a, b, modulo(a, b));
+        }
+
+
+        if (quit() == 1)
+        {
+            return 0;
+        }
+
+    }
+}
+
+void print_result(char c, int a, int b, int d)
+{
+    printf("Result:\n");
+    printf("%i %c %i = %i\n", a, c, b, d);
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int modulo(int a, int b)
+{
+    int rem = a;
+
+    while(rem >= b)
+    {
+        rem = rem - b;
+    }
+
+    return rem;
+}
+
+int division(int a, int b)
+{
+    int rem = a;
+    int counter = 0;
+    while(rem >= b)
+    {
+        rem = rem - b;
+        counter++;
+    }
+    return counter;
+}
+
+int multiply(int a, int b)
+{
+    int result = 0;
+    for (int i = 0; i < a; i++)
+    {
+        result = result + b;
+    }
+    return result;
+}
+
+void mean()
+{
+    int sum = 0;
+    int numbers = get_int("How many values?");
+
+    for (int i = 0; i < 10; i++)
+    {
+        sum += get_int("Value: ");
+    }
+
+
+    printf("Average: %.2f\n", sum / 10.0);
+}
+
+int quit()
+{
+    do
+    {
+        char action = get_char("Continue? y / n? ");
+
+        if(action == 'y' || action == 'Y')
+        {
+            return 0;
+        }
+        if(action == 'n' || action == 'N')
+        {
+            return 1;
+        }
+    }
+    while(true);
+}
+
