04113e0643cc065d81767ba2debebda19189b242
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-02 16:58:19 +0000

    Wed, Nov 2, 2022, 4:58 PM +00:00

--- Section1/Tasks/101/101.c
+++ Section1/Tasks/101/101.c
@@ -13,7 +13,7 @@ int main(void)
     {
         for (int j = 1; j < 11; j++)
         {
-            printf("%i\t", i*j);
+            printf("%i\t", i * j);
         }
         printf("\n");
     }
