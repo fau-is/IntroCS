9b4141d8cfff5ff66b1fc255653d87e6a9452eea
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-02 16:45:14 +0000

    Wed, Nov 2, 2022, 4:45 PM +00:00

--- Section1/Tasks/Condition/calc.c
+++ Section1/Tasks/Condition/calc.c
@@ -6,6 +6,17 @@ int main(void)
     int a = get_int("a: ");
     int b = get_int("b: ");
 
-    
+    if (a > b)
+    {
+        printf("%i\n", a - b);
+    }
+    else if (a < b)
+    {
+        printf("%i\n", a + b);
+    }
+    else
+    {
+        printf("%i\n", a * b);
+    }
 
 }
