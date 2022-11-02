cdfcb5866dbbf3089c559962dbe6c8692b1a1213
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-02 14:09:30 +0000

    Wed, Nov 2, 2022, 2:09 PM +00:00

---
+++ Section1/Class/checksum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+{
+    // Provide a number here
+    int number = -1;
+    do
+    {
+        number = get_int("Positive Number: ");
+
+    }
+    while (number < 0);
+
+    int checksum = 0;
+
+    while(number / 10 > 0 || number % 10 > 0)
+    {
+        int digit = number % 10;
+        checksum = checksum + digit;
+        number = number / 10;
+    }
+
+    printf("Checksum: %i\n", checksum);
+}
