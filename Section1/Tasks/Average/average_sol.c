92d994430559f90a96e41688a4f9508d7f1f90f9
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-02 14:47:32 +0000

    Wed, Nov 2, 2022, 2:47 PM +00:00

--- Section1/Tasks/Average/average_sol.c
+++ Section1/Tasks/Average/average_sol.c
@@ -3,5 +3,11 @@
 
 int main(void)
 {
-    int sum4avg = 
+    int sum4avg = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum4avg += get_int("Number: ");
+    }
+
+    printf("Average: %.2f\n", sum4avg / 10.0);
 }
