73d9875d52870305bdd16093b2ad51e1451cdc28
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-02 16:22:04 +0000

    Wed, Nov 2, 2022, 4:22 PM +00:00

--- Section1/Class/countdown.c
+++ Section1/Class/countdown.c
@@ -4,11 +4,12 @@
 int main(void)
 {
     // Block 1
+    int to_countdown = 0;
     do
     {
         // Block 1.1
-        int to_countdown = get_int("Number: ");
-    } while (to_countdown < 0);
+        to_countdown = get_int("Number: ");
+    } while (to_countdown <= 0);
 
     for(int i = to_countdown; i >= 0; i--)
     {
