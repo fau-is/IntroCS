da4eb91d0f05d590efe0ef350a2d1fd437e23041
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-08 13:59:05 +0000

    Tue, Nov 8, 2022, 1:59 PM +00:00

---
+++ Section2/Tasks/Initials/initials.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string name = get_string("Name: ");
+
+}
+
+
--- Section2/Tasks/Initials/initials_sol.c
+++ Section2/Tasks/Initials/initials_sol.c
@@ -5,15 +5,15 @@
 
 int main(void)
 {
-string s = get_string("Name: ");
+    string name = get_string("Name: ");
 
 
-// Keep track of whether to output next character
-bool output = true;
+    // Keep track of whether to output next character
+    bool output = true;
 
 
 	// Loop over all characters in name
-	for (int i = 0, len = strlen(s); i < len; i++)
+	for (int i = 0, len = strlen(name); i < len; i++)
 	{
 
 		if (output == true)
