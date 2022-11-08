2f8aaf96217181659b6ba059a4c7dca70945dc5d
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-08 13:47:22 +0000

    Some Changes

---
+++ Section2/Tasks/Palindrome/README.md
@@ -0,0 +1,8 @@
+# Palindrome
+
+Write a program palindrome.c that takes a string as input, and determines whether it is a palindrome. This can often be a useful follow-up to reverse.c
+
+Hints:
+- One ‘for’ loop suffices to solve this task
+- strlen(s) gets the length of a string
+- The length of an array is always one larger than the largest index.
--- Section2/Tasks/Palindrome/palindrome.c
+++ Section2/Tasks/Palindrome/palindrome.c
@@ -5,23 +5,7 @@
 int main(void)
 {
     string s = get_string("Text: ");
-    bool palindrome = true;
 
-	for (int i = 0, len = strlen(s); i < len / 2; i++)
-	{
-		if (s[i] != s[len - 1 - i])
-		{
-			palindrome = false;
-		}
-	}
-	// Print output
-	if (palindrome)
-	{
-		printf("PALINDROME\n");
-	}
-	else
-	{
-		printf("NO PALINDROME\n");
-	}
+
 
 }
