c72ed93c95229b4f7776ba18e0d1f91377af67fd
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-08 13:56:43 +0000

    Tue, Nov 8, 2022, 1:56 PM +00:00

---
+++ Section2/Tasks/Initials/initials_sol.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+string s = get_string("Name: ");
+
+
+// Keep track of whether to output next character
+bool output = true;
+
+
+	// Loop over all characters in name
+	for (int i = 0, len = strlen(s); i < len; i++)
+	{
+
+		if (output == true)
+		{
+            printf("%c", toupper(name[i]));
+	        output = false;
+		}
+
+		if (name[i] == ' ')
+		{
+		      output = true;
+		}
+	}
+	printf("\n");
+}
+
+
