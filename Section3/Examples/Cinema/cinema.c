f2fdaf0a540e0ae381ac5f08621f25c93b7e8ed9
Kristof Russmann <91115542+krussmann@users.noreply.github.com>
2022-11-16 12:14:47 +0000

    Wed, Nov 16, 2022, 12:14 PM +00:00

--- Section3/Examples/Cinema/cinema.c
+++ Section3/Examples/Cinema/cinema.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-// Define count of rows and len
-#define ROWS 100
-#define SEATS_pr 9
+// Define count of rows and length of rows
+#define ROWS 4
+#define ROW_LEN 9
+
+// Seats have name, vote count, eliminated status
+typedef struct
+{
+    string name;
+    int votes;
+    bool eliminated;
+}
+candidate;
+
 int main(void)
 {
 
