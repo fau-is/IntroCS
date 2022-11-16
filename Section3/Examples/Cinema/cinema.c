eb540bf45c2d52d626688f82654cbd1e88c44221
Kristof Russmann <91115542+krussmann@users.noreply.github.com>
2022-11-16 13:12:35 +0000

    Wed, Nov 16, 2022, 1:12 PM +00:00

--- Section3/Examples/Cinema/cinema.c
+++ Section3/Examples/Cinema/cinema.c
@@ -13,6 +13,7 @@ typedef struct
     int row;
     int pos;
     bool reserved;
+    string name;
 }
 seat;
 
@@ -22,7 +23,8 @@ seat cinema[ROWS][ROW_LEN];
 // Data extracted from the online processing system
 int data[] = {0, 1, 3, 5, 6, 8, 10, 11, 12, 13, 14, 15};
 string names[] = {"Mark", "Elisa", "Jake", "Scott", "Aiden",
-"Andrew", "Emily", "Charlotte", "Olivia"}
+                  "Andrew", "Emily", "Charlotte", "Olivia",
+                  "Sofia", "Adam", "Tom"};
 
 int main(void)
 {
