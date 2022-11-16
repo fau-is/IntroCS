78e82ba744a07ac79eb13de2652a078402c40835
Kristof Russmann <91115542+krussmann@users.noreply.github.com>
2022-11-16 13:03:17 +0000

    Wed, Nov 16, 2022, 1:03 PM +00:00

--- Section3/Examples/Cinema/cinema.c
+++ Section3/Examples/Cinema/cinema.c
@@ -30,8 +30,8 @@ int main(void)
     // populating the cinema with the data
     for (int i = 0; i < ROWS * ROW_LEN; i++)
     {
-        row = data[reservation] / ROWS;
-        pos = data[reservation] % ROW_LEN;
+        row = data[i] / ROWS;
+        pos = data[i] % ROW_LEN;
 
         // Accessing seat properties for seat in row ... and pos ...
         cinema[row][pos].row = row;
