0d3a49e0b42ab92c1eac96c8b123eaa7bacc769a
Kristof Russmann <91115542+krussmann@users.noreply.github.com>
2022-11-16 12:27:31 +0000

    Wed, Nov 16, 2022, 12:27 PM +00:00

--- Section3/Examples/Cinema/cinema.c
+++ Section3/Examples/Cinema/cinema.c
@@ -16,10 +16,10 @@ seat;
 // Seating rows in the cinema are stored as a ROW x ROW_LEN matrix
 seat cinema[ROWS][ROW_LEN];
 
-//
-
+// Data extracted from the online processing system
+int data = {0, 1, 3, 5, 6, 8, 10, 11, 12, 13, 14, 15}
 
 int main(void)
 {
-
+    
 }
