297ed06fe7b8dba2ae10e636478d0eb4e853ae99
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-29 13:41:03 +0000

    Tue, Nov 29, 2022, 1:41 PM +00:00

---
+++ Section5/Tasks/LinkedList/linked_list.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+typedef struct node
+{
+    int payload;
+    struct node *next;
+}
+node;
+
+node *create(int value);
+node *add(node *list, int value);
+bool find(node *list, int value);
+bool delete(node *list);
+void print_list(node *list);
+
+int main(int argc, char **argv)
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+
+    node *list = create(arr[0]);
+
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    for (int i = 1, len = 7; i < len; i++)
+    {
+        list = add(list, arr[i]);
+    }
+
+    print_list(list);
+}
+
+
+node *create(int value)
+{
+    node *n = malloc(sizeof(node));
+
+    if (n == NULL)
+    {
+        return NULL;
+    }
+
+    n->payload = value;
+    n->next = NULL;
+
+    return n;
+}
+
+
+node* add(node *list, int value)
+{
+    node *new = create(value);
+    if (new != NULL)
+    {
+        new->next = list;
+        return new;
+    }
+    return list;
+}
+
+
+bool find(node *list, int value)
+{
+    for (node* cursor = list; cursor != NULL; cursor = cursor->next)
+    {
+        if (cursor->payload == value)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool delete(node *list)
+{
+    if (list == NULL)
+        return true;
+    delete(list->next);
+    free(list);
+    if(list == NULL)
+        return true;
+    return false;
+}
+
+
+void print_list(node *list)
+{
+    printf("->");
+
+    for (node *cursor = list; cursor != NULL; cursor = cursor->next)
+    {
+        printf("%i->", cursor->payload);
+    }
+    printf("\n");
+
+}
+
+
+
