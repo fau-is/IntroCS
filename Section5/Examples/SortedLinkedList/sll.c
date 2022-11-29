72ca6a059bbda601b42ea6f5ccff4bacd0c325a3
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-29 13:27:49 +0000

    Tue, Nov 29, 2022, 1:27 PM +00:00

--- Section5/Examples/SortedLinkedList/sll.c
+++ Section5/Examples/SortedLinkedList/sll.c
@@ -17,7 +17,20 @@ void print_list(node *list);
 
 int main(int argc, char **argv)
 {
-    printf("Compiles\n");
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+
+    node *list = create(arr[0]);
+
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    for (int i = 1, len = 7; i < len; i ++)
+    {
+        node *list = add(list, arr[i]);
+    }
+
 }
 
 
@@ -37,20 +50,29 @@ node *create(int value)
 }
 
 
-node* add(node* list, int value)
+node* add(node *list, int value)
 {
     node *new = create(value);
     if (new != NULL)
     {
         new->next = list;
-        return true;
+        return new;
     }
-    return false;
-
-
-
+    return list;
 }
 
+
+bool find(node *list, int value)
+{
+    for (node* cursor = list; cursor != NULL; cursor = cursor->next)
+    {
+        if (cursor->payload == value)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
 
 
