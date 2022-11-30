c68468bce5368d1d3503f29e85a2ab2f414716b5
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-30 16:03:57 +0000

    Wed, Nov 30, 2022, 4:03 PM +00:00

--- Section5/Class/linked_list.c
+++ Section5/Class/linked_list.c
@@ -5,10 +5,16 @@ typedef struct node
 {
     int value;
     struct node *next;
-}node;
+} node;
+
+node *create(int value);
+node *add(node *list, int value);
+bool find(node *list, int to_find);
+void delete(node *list);
+
 
 int main(void)
 {
-    
+
 }
 
