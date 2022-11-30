36852b9e0412d87305881438711909bd328de9f4
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-30 16:29:09 +0000

    Wed, Nov 30, 2022, 4:29 PM +00:00

---
+++ Section5/Tasks/LinkedList/linked_list_end.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+typedef struct node
+{
+    int value;
+    struct node *next;
+}
+node;
+
+node *create(int value);
+node *add(node *list, int value);
+bool find(node *list, int value);
+int size(node *list);
+void delete(node *list);
+void print_list(node *list);
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s filepath\n", argv[0]);
+        return 1;
+    }
+
+    FILE *f = fopen(argv[1], "r");
+
+    if (f == NULL)
+    {
+        printf("Path %s does not exist\n", argv[1]);
+        return 2;
+    }
+
+    int i;
+    node *list;
+
+    while(fscanf(f, "%i", &i) == 1)
+    {
+        list = add(list, i);
+        if (list == NULL)
+        {
+            break;
+        }
+    }
+
+    fclose(f);
+    print_list(list);
+    printf("%i numbers were read.\n", size(list));
+    delete(list);
+}
+
+
+node *create(int value)
+{
+    node *new = malloc(sizeof(node));
+
+    if (new != NULL)
+    {
+        new->value = value;
+        new->next = NULL;
+    }
+    return new;
+}
+
+
+node *add(node *list, int value)
+{
+    node *new = create(value);
+
+    if (new == NULL)
+        return list;
+    if (list == NULL)
+    {
+        return new;
+    }
+    else
+    {
+        new->next = list;
+    }
+    return new;
+}
+
+
+bool find(node *list, int value)
+{
+    return false;
+}
+
+void delete(node *list)
+{
+    if (list != NULL)
+    {
+        delete(list->next);
+        free(list);
+    }
+    return;
+}
+
+int size(node *list)
+{
+    int size = 0;
+    for (node *cursor = list; cursor != NULL; cursor = cursor->next)
+    {
+        size++;
+    }
+    return size;
+}
+
+
+void print_list(node *list)
+{
+    printf("->");
+
+    for (node *cursor = list; cursor != NULL; cursor = cursor->next)
+    {
+        printf("%i->", cursor->value);
+    }
+    printf("\n");
+}
+
+
+
