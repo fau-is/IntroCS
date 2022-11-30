bb3841ccba070d5148720c4004d9e2ed763b727c
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-30 08:52:21 +0000

    Wed, Nov 30, 2022, 8:52 AM +00:00

---
+++ Section5/Class/struct.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct node
+{
+    char *s;
+    struct node* next;
+}
+node;
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s [Names]", argv[0]);
+        return 1;
+    }
+
+    for (int i <)
+
+}
