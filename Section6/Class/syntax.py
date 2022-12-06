6b92990df16dc8ea8696bcbd151a6518b749d39a
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-06 12:32:20 +0000

    Tue, Dec 6, 2022, 12:32 PM +00:00

--- Section6/Class/syntax.py
+++ Section6/Class/syntax.py
@@ -3,6 +3,14 @@ from cs50 import get_string
 name = get_string("Name: ")
 
 
-print(f"Hello, {name}")
+
 print("How are you?")
+
+if name == "Sebastian":
+    print(f"Hello, {name}")
+elif name == "Kristof":
+    print("I'm your Tutor")
+else:
+    print("I don't know you")
+
 
