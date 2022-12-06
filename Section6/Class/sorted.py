94ddfaf79118754467e35af8c91c178bbd86e2d3
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-06 12:55:47 +0000

    Tue, Dec 6, 2022, 12:55 PM +00:00

--- Section6/Class/sorted.py
+++ Section6/Class/sorted.py
@@ -1,8 +1,14 @@
-from sys import argv
+from sys import argv, exit
 
 if len(argv) < 3:
     print("Usage..")
+    exit(1)
 
+args = argv[1:]
 arr = []
-for element in argv:
+
+for element in args:
     value = int(element)
+    arr.append(value)
+
+print(sorted(arr))
