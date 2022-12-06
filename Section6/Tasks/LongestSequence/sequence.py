ff107d196132e54ac34ca8fd9647d604e8ab5e92
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-06 13:42:19 +0000

    Tue, Dec 6, 2022, 1:42 PM +00:00

--- Section6/Tasks/LongestSequence/sequence.py
+++ Section6/Tasks/LongestSequence/sequence.py
@@ -4,9 +4,24 @@ def main():
     if len(argv) != 2:
         print(f"Usage: python {argv[0]} filepath")
 
-    with open("numbers.txt", "r") as f:
-        
-        print(f"longest sequence: '{longest_sequence}' with Length: {len_longest_sequence}")
+    with open(argv[1], "r") as f:
+
+        longest_sequence = []
+        tmp_sequence = []
+
+
+        for line in f:
+            for char in line:
+                if str.isdigit(char):
+                    continue
+                if int(char) % 2 == 1:
+                    tmp_sequence.append(char)
+                else:
+                    if len(tmp_sequence) > len(longest_sequence):
+                        longest_sequence = tmp_sequence
+                        tmp_sequence = []
+
+        print(f"longest sequence: '{' '.join(longest_sequence)}' with Length: {len(longest_sequence)}")
 
 
 if __name__ == '__main__':
