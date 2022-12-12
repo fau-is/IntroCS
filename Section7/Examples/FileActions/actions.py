14039597822beb22c991eee73e0de6bc8819a069
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-12 13:47:57 +0000

    Mon, Dec 12, 2022, 1:47 PM +00:00

--- Section7/Examples/FileActions/actions.py
+++ Section7/Examples/FileActions/actions.py
@@ -1,1 +1,52 @@
-class 
+import shutil
+
+
+class Action:
+    def __init__(self, src_path, target_path):
+        self.src_path = src_path
+        self.target_path = target_path
+
+    def do_action(self):
+        pass
+
+    def __str__(self):
+        return f"{self.src_path} to {self.target_path}"
+
+
+class CopyAction(Action):
+    def __init__(self, src_path, target_path):
+        super().__init__(src_path, target_path)
+
+    def do_action(self):
+        try:
+            shutil.copy(self.src_path, self.target_path)
+            return True
+        except:
+            return False
+
+    def __str__(self):
+        return f"Copy {super().__str__()}"
+
+
+class MoveAction(Action):
+    def __init__(self, src_path, target_path):
+        super().__init__(src_path, target_path)
+
+    def do_action(self):
+        try:
+            shutil.move(self.src_path, self.target_path)
+            return True
+        except:
+            return False
+
+    def __str__(self):
+        return f"Move {super().__str__()}"
+
+
+class RenameAction(MoveAction):
+    def __init__(self, src_path, target_path):
+        super().__init__(src_path, target_path)
+
+
+class DeleteAction(Action):
+    
