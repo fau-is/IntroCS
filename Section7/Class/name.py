7cb4649a7034994b6b856697f79c04d2146aa532
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-14 16:10:27 +0000

    Wed, Dec 14, 2022, 4:10 PM +00:00

--- Section7/Class/name.py
+++ Section7/Class/name.py
@@ -8,11 +8,14 @@ class Name:
     def method_name(self):
         print(self.something)
 
+def main():
+    name_1 = Name("IntroCS")
+    name_1.something = "NewName"
 
-name_1 = Name("IntroCS")
+    name_1.method_name()
 
-name_1.something = "NewName"
+if __name__ == "__main__":
+    main()
 
-name_1.method_name()
 
 
