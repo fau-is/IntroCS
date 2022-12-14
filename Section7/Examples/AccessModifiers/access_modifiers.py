39103e3de226e10392145fc2723aefab678e80a2
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-14 13:55:08 +0000

    Wed, Dec 14, 2022, 1:55 PM +00:00

--- Section7/Examples/AccessModifiers/access_modifiers.py
+++ Section7/Examples/AccessModifiers/access_modifiers.py
@@ -1,11 +1,11 @@
 class AccessModifier:
-    def __init__(self, name):
+    def __init__(self):
         self.__value = "Private"
         self._value = "Protected"
         self.value = "Public"
 
     # Getter
-    def get_access_modifier(self):
+    def get_value(self):
         return self.__value
 
     def set_value(self, value):
@@ -13,7 +13,7 @@ class AccessModifier:
             self.__value = value
 
 if __name__ == "__main__":
-    t = Test("IntroCS")
+    t = AccessModifier()
     # Private and protected variables are easily accessible.
     print(t.value)
     print(t._value)
@@ -21,11 +21,13 @@ if __name__ == "__main__":
     # Toggle comment of the following two lines.
     # The first one leads to an error,
     # the second line will work:
-    print(t.__value)
-    # print(t.get_value())
+    # print(t.__value)
+    print(t.get_value())
 
     t.set_value(12)
+    # Setter protects setting integer as value
     print(t.get_value())
+    # Setting to string will work.
     t.set_value("Hello World")
     print(t.get_value())
 
