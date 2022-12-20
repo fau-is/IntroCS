8a3028fe9325fcbbb0d8ea033c689db94be00c95
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-20 13:47:48 +0000

    Tue, Dec 20, 2022, 1:47 PM +00:00

--- Section8/Tasks/Symmetry/symmetry_test.py
+++ Section8/Tasks/Symmetry/symmetry_test.py
@@ -1,10 +1,9 @@
 import unittest
-from graph_drawer import draw_graph_from_mat
+from symmetry import is_matrix_symmetric
 
 class TestDetectsUnsymmetricMatrix(unittest.TestCase):
     def setUp(self):
-        self.matrices =
-        [
+        self.matrices = [
             [
                 [0, 1, 1, 1],
                 [1, 0, 0, 0],
@@ -20,8 +19,8 @@ class TestDetectsUnsymmetricMatrix(unittest.TestCase):
 
         ]
 
-    def test_non_symmetric():
-
-
+    def test_non_symmetric(self):
+        self.assertFalse(is_matrix_symmetric(self.matrices[0]))
 
     def test_with_weights():
+        self.assertFalse(is_matrix_symmetric(self.matrices[1]))
