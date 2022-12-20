1bd07119bb0ac315b576a629f6151a12ab8b75b2
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-20 10:28:08 +0000

    Tue, Dec 20, 2022, 10:28 AM +00:00

---
+++ Section8/Class/binary_tree.py
@@ -0,0 +1,19 @@
+from graphviz import Graph
+
+def print_array_as_bt(arr):
+    g = Graph()
+    g.attr('node', fillcolor='white', fontcolor='black', shape='circle', style='filled', border='black')
+    for node in arr:
+        g.node(str(node), str(node))
+
+    for i in range(len(arr)):
+        if ((2 * i) + 1) < len(arr):
+            g.edge(str(arr[i]), str(arr[(2 * i) + 1]))
+        if ((2 * i) + 2) < len(arr):
+            g.edge(str(arr[i]), str(arr[(2 * i) + 2]))
+
+    g.render(filename="bt", format="png")
+
+
+if __name__ == '__main__':
+    arr = [18, 13, 35, 12, 15]
