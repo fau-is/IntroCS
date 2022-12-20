1b8bb54505d7f719db8056d661680d27e3054683
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-20 13:05:26 +0000

    Tue, Dec 20, 2022, 1:05 PM +00:00

--- Section8/Class/obj_tree.py
+++ Section8/Class/obj_tree.py
@@ -1,4 +1,4 @@
-class BinTreeNode():
+class BinSearchTreeNode():
     def __init__(self, value):
         self.__value = value
         self.__left = None
@@ -35,9 +35,32 @@ class BinTree():
     def __init__(self):
         self.root = None
 
+    def tree_drawer(self):
+        g = Graph()
+        g.attr('node', fillcolor='white', fontcolor='black', shape='circle', style='filled', border='black')
+        BinTree.__rec_helper(self.root, g)
+        g.render(filename="bto", format="png")
+
     @classmethod
     def create_bin_tree(cls, value):
         tree = cls()
         tree.root = BinTreeNode(value)
         return tree
+
+    @staticmethod
+    def __rec_helper_print(node, g):
+        if node == None:
+            return
+
+        g.node(str(node.value), str(node.value))
+        if node.left:
+            BinTree.__rec_helper(node.left, g)
+            g.edge(str(node.value), str(node.left.value))
+        if node.right:
+            BinTree.__rec_helper(node.right, g)
+            g.edge(str(node.value), str(node.right.value))
+
+
+
+
 
