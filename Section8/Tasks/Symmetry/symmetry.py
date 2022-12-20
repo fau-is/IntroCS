aea5f2e141545b366721adb8b22c0360264d478c
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-20 13:51:43 +0000

    Tue, Dec 20, 2022, 1:51 PM +00:00

--- Section8/Tasks/Symmetry/symmetry.py
+++ Section8/Tasks/Symmetry/symmetry.py
@@ -1,7 +1,2 @@
 def is_matrix_symmetric(matrix):
-    symmetric = True
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            if matrix[i][j] != matrix[j][i]:
-                symmetric = False
-    return symmetric
+    pass
