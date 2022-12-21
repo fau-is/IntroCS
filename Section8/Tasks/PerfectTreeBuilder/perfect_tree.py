ab75a899fc179c95b985c07db48da85758c88a46
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-21 09:51:17 +0000

    Wed, Dec 21, 2022, 9:51 AM +00:00

--- Section8/Tasks/PerfectTreeBuilder/perfect_tree.py
+++ Section8/Tasks/PerfectTreeBuilder/perfect_tree.py
@@ -8,7 +8,7 @@ class BinTreeNode:
         if not self.left and not self.right:
             return 0
         if self.left and self.right:
-            return max(self.right.get_height(), self.left.get_height()) + 1
+            return min(self.right.get_height(), self.left.get_height()) + 1
         if self.right and not self.left:
             return self.right.get_height() + 1
         else:
@@ -16,4 +16,24 @@ class BinTreeNode:
 
     def add_node(self, value)
         if self.left is None:
-            self.left
+            self.left = BinTreeNode(value)
+        elif self.right is None:
+            self.left = BinTreeNode(value)
+        elif self.left.get_height() < self.right.get_height():
+            self.left.add_node(value)
+        else:
+            self.right.add_node
+
+    def print_preorder(self):
+        print(self.value)
+        if self.left:
+            self.left.print_preorder()
+        if self.right:
+            self.right.print_preorder()
+
+class BinTree:
+    def __init__(self, value):
+        self.root = BinTreeNode(value)
+
+    def add_node(self, value):
+        self.root 
