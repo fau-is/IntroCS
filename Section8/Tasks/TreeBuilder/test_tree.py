ca820707892ede90aff3274c7cb4f305590ec18d
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-21 14:45:51 +0000

    Wed, Dec 21, 2022, 2:45 PM +00:00

--- Section8/Tasks/TreeBuilder/test_tree.py
+++ Section8/Tasks/TreeBuilder/test_tree.py
@@ -1,7 +1,7 @@
 import unittest
 from tree import BinTree, BinTreeNode
 
-def tree_builder1():
+def tree_builder_root_right_missing():
     root = BinTreeNode("Root")
     root.left = BinTreeNode("RootLeft")
     root.left.left = BinTreeNode("RootLeftLeft")
@@ -9,9 +9,26 @@ def tree_builder1():
 
     return root
 
+def tree_builder_root_left_right_left_missing():
+    root = BinTreeNode("Root")
+    root.left = BinTreeNode("RootLeft")
+    root.left.left = BinTreeNode("RootLeftLeft")
+    root.left.left.left = BinTreeNode("RootLeftLeftLeft")
+    root.left.left.right = BinTreeNode("RootLeftLeftRight")
+    root.left.right.right = BinTreeNode("RootLeftRightRight")
+    root.right = BinTreeNode("RootRight")
+    root.right.right = BinTreeNode("RootRightRight")
+    root.right.right.left = BinTreeNode("RootRightRightLeft")
+    root.right.right.right = BinTreeNode("RootRightRightRight")
+    root.right.left = BinTreeNode("RootRightLeft")
+    root.right.left.left = BinTreeNode("RootRightLeftLeft")
+    root.right.left.right = BinTreeNode("RootRightLeftRight")
+    return root
+
 class TestTreeBuilder(unittest.TestCase):
     def setUp(self):
-        self.root = tree_builder1()
+        self.root_right_missing = tree_builder_root_right_missing()
+        self.root_left_right_left_missing = tree_builder_root_left_right_left_missing()
 
     def test_create_root(self):
         tree = BinTree("Root")
@@ -28,8 +45,12 @@ class TestTreeBuilder(unittest.TestCase):
         self.assertEqual("RootRight", tree.root.right.value)
 
     def test_add_node_to_filled_tree(self):
-        self.tree.add_node("RootRight")
-        self.assertEqual("RootRight", self.root.right.value)
+        self.root_right_missing.add_node("RootRight")
+        self.assertEqual("RootRight", self.root_right_missing.right.value)
+
+    def test_add_node_to_filled_tree(self):
+        self.root_left_right_left_missing.add_node("RootLeftRightLeft")
+        self.assertEqual("RootLeftRightLeft", self.root_left_right_left_missing.left.right.left.value)
 
 
 
