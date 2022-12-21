57225fa52e5cbdeefceb1ff0a3ba922e5e279100
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-21 14:22:17 +0000

    Wed, Dec 21, 2022, 2:22 PM +00:00

--- Section8/Tasks/TreeBuilder/test_tree.py
+++ Section8/Tasks/TreeBuilder/test_tree.py
@@ -2,8 +2,24 @@ import unittest
 from tree import BinTree, BinTreeNode
 
 def tree_builder1():
-    tree = BinTree()
+    root = BinTreeNode("Root")
+    root.left = BinTreeNode("RootLeft")
+    root.left.left = BinTreeNode("RootLeftLeft")
+    root.left.right = BinTreeNode("RootLeftRight")
 
 class TestTreeBuilder(unittest.TestCase):
     def setUp(self):
+        self.tree = tree_builder1()
+
+    def test_create_root(self):
+        tree = BinTree("Root")
+
+        self.assertEqual("Root", tree.root.value)
+
+    def test_add_nodes_to_empty_tree():
+        self.tree = BinTree("Root")
+        
+
+if __name__ == "__main__":
+    unittest.main()
 
