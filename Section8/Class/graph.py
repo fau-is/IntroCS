61c8703225c137ed134da84e60f473380962c45d
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-12-21 12:39:43 +0000

    Wed, Dec 21, 2022, 12:39 PM +00:00

---
+++ Section8/Class/graph.py
@@ -0,0 +1,28 @@
+class GraphNode:
+    def __init__(self, data):
+        self.neighbors = []
+        self.data = data
+
+    def dfs_rec(self, visited):
+        if self not in visited:
+            visited.append(self)
+            for neighbor in self.neighbors:
+                visited = neighbor.dfs_rec(visited)
+        return visited
+
+    def add_neighbor(self, neighbor):
+        self.neighbors.append(neighbor)
+
+class Graph():
+    def __init__(self):
+        self.nodes = {}
+        self.adj_matrix = {}
+
+    def add_node(self, data):
+        g_node = GraphNode(data)
+        self.nodes[data] = g_node
+        self.adj_matrix[data] = []
+
+    def add_edge(self, source, target):
+        
+
