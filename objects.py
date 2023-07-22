24e12c4ccdf65c4a90867b64074a77cecd12dbcc
guenni-kologe <laurin@praxis-schmid.com>
2023-07-22 13:23:36 +0200

    User back in objects

--- objects.py
+++ objects.py
@@ -9,8 +9,38 @@ mastodon = Mastodon(
 )
 
 
+class User:
+    # list of all 'loaded' users
+    users = []
+
+    # Constructor
+    def __init__(self, name, id, display_name, followers_count): #signdate):
+        self.name = name
+        self.id = id
+        self.display_name = display_name
+        self.followers_count = followers_count
+        #self.signdate = signdate
+        User.users.append(id)
+
+    # getter-Methods
+    def get_name(self):
+        return self.name
+
+    def get_id(self):
+        return self.id
+
+    def get_display_name(self):
+        return self.display_name
+
+    def get_followers_count(self):
+        return self.followers_count
+
+    #def get_signdate(self):
+        #return self.signdate
+        
+
 class Toot:
-    def __init__(self, content, account, user_id, hashtags, bookmark, no_replies, url, toot_id, count_replies):
+    def __init__(self, content, account, user_id, hashtags, bookmark, no_replies, url, toot_id, count_replies, pubdate):
         self.content = content
         self.account = account
         self.user_id = user_id
@@ -20,6 +50,7 @@ class Toot:
         self.url = url
         self.toot_id = toot_id
         self.count_replies = count_replies
+        self.pubdate = pubdate
         
     def get_replies(self, x):
         toot_id = self.toot_id
