45a32c79bad5f657c9606f666e983470664faa6f
JobstHanna <hanna-jobst@gmx.de>
2023-08-21 15:54:52 +0200

    tests & resources

---
+++ tests/test_loadtoots.py
@@ -0,0 +1,12 @@
+import unittest
+from IntroCS.MastodonOOP import MastotonOOP
+import json
+
+
+class Test_loadtoots(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
