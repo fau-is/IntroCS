4da3d68d54ccefc2fa0c0d91a279c07636b3fcbd
JobstHanna <hanna-jobst@gmx.de>
2023-08-24 11:22:46 +0200

    tests

---
+++ tests/resources/toots.py
@@ -0,0 +1,13 @@
+import json
+
+toot_media = {
+    'Name': 'John', 'Alter': 30, 'Beruf': 'Ingenieur'
+}
+
+
+mock daten für toot class
+1. haben wir richtige attribute 
+2. dict richtig gelesen ? load function
+
+
+alles muss überprüft werden 
--- tests/test_MediaTriggers.py
+++ tests/test_MediaTriggers.py
@@ -1,52 +1,107 @@
 import unittest
-from IntroCS.MastodonOOP import MastodonOOP
-import json
+import IntroCS.MastodonOOP
 
 
 class Test_mediaTriggers(unittest.TestCase):
     
     def setUp(self):
-        pass
+        toot_true = MastodonOOP.Toot (
+            account = '',
+            toot_id = '',
+            content = '',
+            user_id = '',
+            hashtags = '',
+            bookmark = '',
+            no_replies = '',
+            url = '',
+            count_replies = '',
+            pubdate = '',
+            mentions = '',
+            media = '',
+            language = '',
+            poll = ''        
+        )
+        toot_false = MastodonOOP.Toot (
+            account = '',
+            toot_id = '',
+            content = '',
+            user_id = '',
+            hashtags = '',
+            bookmark = '',
+            no_replies = '',
+            url = '',
+            count_replies = '',
+            pubdate = '',
+            mentions = '',
+            media = '',
+            language = '',
+            poll = ''        
+        )
     
     def tearDown(self):
         pass
     
     def test_MediaTrigger(self):
-        #self.assertEqual(MastodonOOP.MediaTrigger.evaluate(toot_Media), True)
-        self.assertTrue(MastodonOOP.MediaTrigger.evaluate(toot_media))
-        self.assertFalse(MastodonOOP.MediaTrigger.evaluate(toot_no_media))
+        toot_true = {"media": True}
+        toot_false = {"media": False}
+        
+        self.assertTrue(MastodonOOP.MediaTrigger.evaluate(toot_true))
+        self.assertFalse(MastodonOOP.MediaTrigger.evaluate(toot_false))
 
     def test_VideoMediaTrigger(self):
-        self.assertTrue(MastodonOOP.VideoMediaTrigger.evaluate(toot_video))
-        self.assertFalse(MastodonOOP.VideoMediaTrigger.evaluate(toot_no_video))
+        # wie funktioniert der filter ???
+        # toot_true = {content["type"] = "video" }
+        toot_false = {"media": False}
+        
+        #self.assertTrue(MastodonOOP.VideoMediaTrigger.evaluate(toot_true))
+        self.assertFalse(MastodonOOP.VideoMediaTrigger.evaluate(toot_false))
         
     def test_GifMediaTrigger(self):
-        self.assertTrue(MastodonOOP.GifMediaTrigger.evaluate(toot_gif))
-        self.assertFalse(MastodonOOP.GifMediaTrigger.evaluate(toot_no_gif))
+        toot_true = {"media": True}
+        toot_false = {"media": False}
+        
+        self.assertTrue(MastodonOOP.GifMediaTrigger.evaluate(toot_true))
+        self.assertFalse(MastodonOOP.GifMediaTrigger.evaluate(toot_false))
         
     def test_AudioMediaTrigger(self):
-        self.assertTrue(MastodonOOP.AudioMediaTrigger.evaluate(toot_audio))
-        self.assertFalse(MastodonOOP.AudioMediaTrigger.evaluate(toot_no_audio))
+        toot_true = {"media": True}
+        toot_false = {"media": False}
+        
+        self.assertTrue(MastodonOOP.AudioMediaTrigger.evaluate(toot_true))
+        self.assertFalse(MastodonOOP.AudioMediaTrigger.evaluate(toot_false))
         
     def test_LanguageTrigger(self):
         english = MastodonOOP.NotTrigger("en")
         
-        self.assertTrue(english.evaluate(toot_eng))
-        self.assertFalse(english.evaluate(toot_no_eng))  
+        toot_true = {"language": "en"}
+        toot_false = {"language": "du"}
+        
+        self.assertTrue(english.evaluate(toot_true))
+        self.assertFalse(english.evaluate(toot_false))  
     
     def test_PollTrigger(self):
-        self.assertTrue(MastodonOOP.PollTrigger.evaluate(toot_poll))
-        self.assertFalse(MastodonOOP.PollTrigger.evaluate(toot_no_poll))
+        # poll_filter = PollTrigger()
+        toot_true = {"media": True}
+        toot_false = {"media": False}
+        
+        self.assertTrue(MastodonOOP.PollTrigger.evaluate(toot_true))
+        self.assertFalse(MastodonOOP.PollTrigger.evaluate(toot_false))
         
     def test_MentionsTrigger(self):
-        self.assertTrue(MastodonOOP.MentionsTrigger.evaluate(toot_mention))
-        self.assertFalse(MastodonOOP.MentionsTrigger.evaluate(toot_no_mention))
+        toot_true = {"media": True}
+        toot_false = {"media": False}
+        
+        self.assertTrue(MastodonOOP.MentionsTrigger.evaluate(toot_true))
+        self.assertFalse(MastodonOOP.MentionsTrigger.evaluate(toot_false))
         
     def test_PhraseTrigger(self):
-        phrase = MastodonOOP.NotTrigger("dog")
+        phrase = MastodonOOP.PhraseTrigger("dog")
+
+        toot_true = {"content": "dog"}
+        toot_false = {"content": "sun"}
         
-        self.assertTrue(phrase.evaluate(toot_dog))
-        self.assertFalse(phrase.evaluate(toot_no_dog)) 
+        self.assertTrue(phrase.evaluate(toot_true))
+        self.assertFalse(phrase.evaluate(toot_false)) 
     
 if __name__ == '__main__':
     unittest.main()
