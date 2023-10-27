ec60f9c963606272ab02206645fa0c4f32871b2b
JobstHanna <hanna-jobst@gmx.de>
2023-10-27 16:16:54 +0200

    test

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def test_API():
     
     if result.exit(0):
         check50.log("Your API-Initiation works correctly.")
-    if not result:
+    else:
         raise check50.Failure("Your API-Initiation does not correctly work, check again if you are missing anything!")
 
 @check50.check()
