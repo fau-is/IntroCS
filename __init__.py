e38548040fdf9876d5594ce2d8c07c2fcc5fe224
JobstHanna <hanna-jobst@gmx.de>
2023-10-31 13:53:53 +0100

    test

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ def test_API():
     if not result:
         raise check50.Mismatch(expected, result, "Your API-Initiation does not correctly work, check again if you are missing anything!")
     
-    raise check50.Mismatch(expected, result, "Your API-Initiation works correctly!")
+    raise check50.log(expected, result, "Your API-Initiation works correctly!")
 
 @check50.check()
 def test_Toot():
