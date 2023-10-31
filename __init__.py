ab686fe09e3ee3838729e2f09cee4987caf6eae7
JobstHanna <hanna-jobst@gmx.de>
2023-10-31 14:42:01 +0100

    test

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def test_API():
     expected = ""
     
     if result:
-        raise check50.Mismatch(result, expected, "Your API-Initiation does not correctly work, check again if you are missing anything!").exit(1)
+        raise check50.Mismatch(result, expected, "Your API-Initiation does not correctly work, check again if you are missing anything!")
     
     check50.log("Your API-Initiation works correctly!")
 
