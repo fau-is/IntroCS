28df685903032c1c306ce6c37ca8083ee50be940
JobstHanna <hanna-jobst@gmx.de>
2023-11-10 10:42:14 +0100

    test

--- __init__.py
+++ __init__.py
@@ -2,17 +2,56 @@ import check50
 import check50.py
 from mastodon import Mastodon
 
-def import_toots():
-    toots = check50.py.import_("toots.py")
-    if toots is None:
-        raise check50.Failure("Toot Import failed")
-    return toots.toot_true, toots.toot_false
+
 
 def import_mastodon():
     mastodon = check50.py.import_("MastodonOOP.py")
     if mastodon is None:
         raise check50.Failure("Mastodon Import failed")
     return mastodon
+
+def import_toots(import_mastodon):
+    Masto = import_mastodon()
+    toot_true = Masto.Toot (
+            account = [{"id": 123, "username": "Marco"}],
+            toot_id = True,
+            content = '<p>Hello from Python, dog</p>',
+            user_id = True,
+            hashtags = [{'name': 'dog', 'url': True, 'history': ''}],
+            bookmark = True,
+            no_replies = True,
+            url = True,
+            count_replies = True,
+            pubdate = '2022-07-22 09:37:34+00:00',
+            mentions = True,
+            media = [{"id": 123,"type":"image"},
+                     {"id": 1234,"type":"video"},
+                     {"id": 12345,"type":"gifv"},
+                     {"id": 123456,"type":"audio"},
+                     {"id": 1234567,"type":"unknown"} 
+                     ],
+            language = 'en',
+            poll = True        
+        )
+
+    toot_false = Masto.Toot (
+            account = '',
+            toot_id = '',
+            content = 'sun',
+            user_id = '',
+            hashtags = '',
+            bookmark = '',
+            no_replies = '',
+            url = '',
+            count_replies = '',
+            pubdate = '2024-07-22 09:37:34+00:00',
+            mentions = False,
+            media = '',
+            language = '',
+            poll = False        
+        )
+        
+    return toot_true, toot_false
 
 @check50.check()
 def test_file_exists():
