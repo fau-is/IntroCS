2e548fb9278152c205429003fcf3f86d77d67143
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2023-11-22 10:16:36 +0000

    Wed, Nov 22, 2023, 11:16 AM +01:00

--- Section4/Class/string.c
+++ Section4/Class/string.c
@@ -4,19 +4,7 @@
 
 int main(void)
 {
-    char *s = "Hello";
-    char t[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-
-    char *u = malloc(sizeof(char) * 100);
-
-    if (u == NULL)
-    {
-        return 1;
-    }
-
-    scanf("%s", u);
-
-    printf("%s\n", u);
+    
 
 
 }
