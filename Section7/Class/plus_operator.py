676a44ad4497bd7a6f74ae18d4a41123a9ad7bb8
Xin <147514981+WeixinWang24@users.noreply.github.com>
2023-12-13 16:32:44 +0000

    Wed, Dec 13, 2023, 5:32 PM +01:00

--- Section7/Class/plus_operator.py
+++ Section7/Class/plus_operator.py
@@ -1,17 +1,19 @@
 if __name__ == "__main__":
+    a1 = ["list1"]
+    a2 = ["list2"]
+    a3 = a1 + a2
+    print("a3: ", a3)
 
-a1 = ["list1"]
-a2 = ["list2"]
-a3 = a1 + a2
-print("a3: ", a3)
+    s1 = "a"
+    s2 = "b"
+    s3 = s1 + s2
+    print("s3: ", s3)
 
-s1 = "a"
-s2 = "b"
-s3 = s1 + s2
-print("s3: ", s3)
+    n1 = 1
+    n2 = 2
+    n3 = n1 + n2
+    print("n3: ", n3)
 
-n1 = 1
-n2 = 2
-n3 = n1 + n2
-print("n3: ", n3)
+    test = s1 + n2
+    print("test: ", test)
 
