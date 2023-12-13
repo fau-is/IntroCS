c156d44bfb2a661a8195c592d3e6dc759534f22e
Xin <147514981+WeixinWang24@users.noreply.github.com>
2023-12-13 16:30:32 +0000

    Wed, Dec 13, 2023, 5:30 PM +01:00

--- Section7/Class/plus_operator.py
+++ Section7/Class/plus_operator.py
@@ -1,9 +1,17 @@
+if __name__ == "__main__":
+
 a1 = ["list1"]
 a2 = ["list2"]
-
 a3 = a1 + a2
+print("a3: ", a3)
 
 s1 = "a"
 s2 = "b"
 s3 = s1 + s2
+print("s3: ", s3)
+
+n1 = 1
+n2 = 2
+n3 = n1 + n2
+print("n3: ", n3)
 
