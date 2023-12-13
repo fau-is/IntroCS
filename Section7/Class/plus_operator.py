5ebbdf9cda1d17bf3d9dc6c1c03c45adc446b506
Xin <147514981+WeixinWang24@users.noreply.github.com>
2023-12-13 16:52:28 +0000

    Wed, Dec 13, 2023, 5:52 PM +01:00

--- Section7/Class/plus_operator.py
+++ Section7/Class/plus_operator.py
@@ -1,15 +1,25 @@
 class person:
-    def __init__(self, name):
+    def __init__(self, name, height, ideal_partner_height):
         self.__name = name
+        self.__height = height
+        self.__ideal_partner_height = ideal_partner_height
 
     def get_name(self):
         return self.__name
+
+    def get_height(self):
+        return self.__height
 
     def __add__(self, person):
         if type(person) is type(self):
             return self.__name + person.get_name()
 
-    def __mul__(sel)
+    def __mul__(self, person):
+        if type(person) is type(self):
+            if self.__ideal_partner_height < person.get_height():
+                return str(self.__name + " likes " + person.get_name())
+            else:
+                return "too bad, maybe next time"
 
 
 if __name__ == "__main__":
@@ -28,8 +38,8 @@ if __name__ == "__main__":
     n3 = n1 + n2
     print("n3: ", n3)
 
-    p1 = person("Peter")
-    p2 = person("Lucas")
-    test = p1 + p2
+    p1 = person("Peter", 180, 170)
+    p2 = person("Marie", 175, 160)
+    test = p1 * p2
     print("test: ", test)
 
