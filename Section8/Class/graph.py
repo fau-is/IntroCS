f8ea7c8336afd24c5f068c483cc53ec2badc3285
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2023-12-18 14:54:47 +0000

    Mon, Dec 18, 2023, 3:54 PM +01:00

--- Section8/Class/graph.py
+++ Section8/Class/graph.py
@@ -4,7 +4,35 @@ class Graph(dict)
     def __init__():
         super().__init__()
 
-    def add_edge(self, a: str, b: str):
-        self[a] = b
+    def add_node(n):
+        self[n] = []
 
-        
+    def add_edge(self, a: str, b: str):
+        if a not in self:
+            self.add_node(a)
+
+        self[a].append(b)
+        self[a] = sorted(self[a])
+
+
+    def dfs(self, s: str):
+        if s not in self:
+            raise ValueError("Node does not exist")
+
+        stack = [s]
+        visited = []
+
+        while stack:
+            node = stack.pop()
+            visited.append(node)
+            for neighbor in self[node]:
+                stack.append(neighbor)
+                
+
+
+    def bfs()
+
+
+
+
+
