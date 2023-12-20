f4a4c5497b4d443147a560e19b2a3a37c4a83f35
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2023-12-20 14:18:53 +0000

    Wed, Dec 20, 2023, 3:18 PM +01:00

--- Section8/Tasks/Symmetry/symmetry.py
+++ Section8/Tasks/Symmetry/symmetry.py
@@ -1,2 +1,10 @@
 def is_matrix_symmetric(matrix):
-    pass
+    if len(matrix) != len(matrix[0]):
+        return False
+    else:
+        for i in range len(matrix):
+            for j in range len(matrix[i]):
+                if matrix[i] != matrix[j]:
+                    return False
+
+    return True
