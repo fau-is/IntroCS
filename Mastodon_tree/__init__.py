25c14da8fc6fa29c8586ef1e0d24a1fd02bafb6e
jfilleboeck <49759785+jfilleboeck@users.noreply.github.com>
2023-12-20 14:07:41 +0100

    Update __init__.py

--- Mastodon_tree/__init__.py
+++ Mastodon_tree/__init__.py
@@ -82,8 +82,8 @@ def tree_structure():
         raise check50.Failure("Tree structure validation failed.")
 
 @check50.check(tree_structure)
-def linear_search():
-    """Linear search works correctly"""
+def iterative_search():
+    """Iterative search works correctly"""
     bst_module = import_bst()
     usernames = ['THD_IT', 'Harmonia_Amanda', 'Dju', 'GeoffreyDorne', 'Bram_Finkel',
                  'HyP', 'mistur', 'Zestryon', 'BrunoBellamy']
@@ -91,20 +91,20 @@ def linear_search():
         bst_module.add("Lopinel")
         for username in usernames:
             bst_module.add(username)
-        if bst_module.linear_search('Lopinel').username != 'Lopinel':
-            raise check50.Mismatch('Lopinel', bst_module.linear_search('Lopinel').username)
-        if bst_module.linear_search('mistur').username != 'mistur':
-            raise check50.Mismatch('mistur', bst_module.linear_search('mistur').username)
-        if bst_module.linear_search('NonExistingUser'):
+        if bst_module.iterative_search(bst_module.root, 'Lopinel').username != 'Lopinel':
+            raise check50.Mismatch('Lopinel', bst_module.iterative_search(bst_module.root, 'Lopinel').username)
+        if bst_module.iterative_search(bst_module.root, 'mistur').username != 'mistur':
+            raise check50.Mismatch('mistur', bst_module.iterative_search(bst_module.root, 'mistur').username)
+        if bst_module.iterative_search('NonExistingUser'):
             raise check50.Mismatch('False', 'True')
         bst_module.root = None
-        if bst_module.linear_search('Lopinel'):
+        if bst_module.iterative_search(bst_module.root, 'Lopinel'):
             raise check50.Mismatch('False', 'True')
     except:
         raise check50.Failure("Linear search failed.")
 
-@check50.check(linear_search)
-def binary_search():
+@check50.check(iterative_search)
+def recursive_search():
     """Binary search works correctly"""
     bst_module = import_bst()
     usernames = ['THD_IT', 'Harmonia_Amanda', 'Dju', 'GeoffreyDorne', 'Bram_Finkel',
@@ -113,19 +113,19 @@ def binary_search():
         bst_module.add("Lopinel")
         for username in usernames:
             bst_module.add(username)
-        if bst_module.binary_search(bst_module.root, 'Lopinel').username != 'Lopinel':
-            raise check50.Mismatch('Lopinel', bst_module.binary_search(bst_module.root, 'Lopinel').username)
-        if bst_module.binary_search(bst_module.root, 'mistur').username != 'mistur':
-            raise check50.Mismatch('mistur', bst_module.binary_search(bst_module.root, 'mistur').username)
-        if bst_module.binary_search(bst_module.root, 'NonExistingUser'):
+        if bst_module.recursive_search(bst_module.root, 'Lopinel').username != 'Lopinel':
+            raise check50.Mismatch('Lopinel', bst_module.recursive_search(bst_module.root, 'Lopinel').username)
+        if bst_module.recursive_search(bst_module.root, 'mistur').username != 'mistur':
+            raise check50.Mismatch('mistur', bst_module.recursive_search(bst_module.root, 'mistur').username)
+        if bst_module.recursive_search(bst_module.root, 'NonExistingUser'):
             raise check50.Mismatch('False', 'True')
         bst_module.root = None
-        if bst_module.binary_search(bst_module.root, 'Lopinel'):
+        if bst_module.recursive_search(bst_module.root, 'Lopinel'):
             raise check50.Mismatch('False', 'True')
     except:
         raise check50.Failure("Binary search failed.")
 
-@check50.check(binary_search)
+@check50.check(recursive_search)
 def preorder():
     """Preorder traversal returns expected order of usernames"""
     bst_module = import_bst()
